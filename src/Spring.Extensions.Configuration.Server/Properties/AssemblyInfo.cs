29181aa5d7e73d211a46db6290cc030a1c82c34a
Dave Tillman <dtillman@pivotal.io>
2016-01-15 11:07:55 -0800

    Project file tidy

--- src/Spring.Extensions.Configuration.Server/Properties/AssemblyInfo.cs
+++ src/Spring.Extensions.Configuration.Server/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Spring.Extensions.Configuration.Server")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Original Authors 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
