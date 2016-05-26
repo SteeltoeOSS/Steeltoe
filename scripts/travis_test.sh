01429adca84f8920cf8ee6f42d1a1c86b436be78
Dave Tillman <dtillman@pivotal.io>
2016-05-26 13:02:46 -0600

    Use correct framework in travis_test

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -3,9 +3,9 @@ export DOTNET_INSTALL_DIR="$PWD/.dotnetsdk"
 export PATH="$DOTNET_INSTALL_DIR:$PATH"
 # Run unit tests 
 cd test/SteelToe.Extensions.Configuration.CloudFoundry.Test
-dotnet test --framework netstandard1.3
+dotnet test --framework netcoreapp1.0
 cd ../..
 cd test/SteelToe.Extensions.Configuration.ConfigServer.Test
-dotnet test --framework netstandard1.3
+dotnet test --framework netcoreapp1.0
 cd ../..
 
