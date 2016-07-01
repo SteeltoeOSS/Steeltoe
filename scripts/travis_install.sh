9f7141eab88eee888ea80e6a784c15b4faca43f0
Dave Tillman <dtillman@pivotal.io>
2016-07-01 09:50:37 -0600

    Update OpenSSL during install

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew install openssl
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew link --force openssl
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then export DOTNET_SDK_URL=https://go.microsoft.com/fwlink/?LinkID=809128 ; fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then export DOTNET_SDK_URL=https://go.microsoft.com/fwlink/?LinkID=809129 ; fi   
 export DOTNET_INSTALL_DIR="$PWD/.dotnetsdk"
@@ -6,5 +9,3 @@ mkdir -p "$DOTNET_INSTALL_DIR"
 curl -L "$DOTNET_SDK_URL" | tar -xzv -C "$DOTNET_INSTALL_DIR"
 export PATH="$DOTNET_INSTALL_DIR:$PATH"
 dotnet --info
-openssl versions
-brew --version
