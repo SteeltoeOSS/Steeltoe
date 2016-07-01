15796e640bce4c3cd6bb76fc656156b14289470d
Dave Tillman <dtillman@pivotal.io>
2016-07-01 11:40:06 -0600

    Fixup travis build

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update ; fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew install openssl ; fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew link --force openssl ; fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then export DOTNET_SDK_URL=https://go.microsoft.com/fwlink/?LinkID=809128 ; fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then export DOTNET_SDK_URL=https://go.microsoft.com/fwlink/?LinkID=809129 ; fi   
 export DOTNET_INSTALL_DIR="$PWD/.dotnetsdk"
@@ -6,3 +9,4 @@ mkdir -p "$DOTNET_INSTALL_DIR"
 curl -L "$DOTNET_SDK_URL" | tar -xzv -C "$DOTNET_INSTALL_DIR"
 export PATH="$DOTNET_INSTALL_DIR:$PATH"
 dotnet --info
+npm install jsonfile -g
