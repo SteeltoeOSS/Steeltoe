a71b6026857eca253e20235d864859937756154d
Dave Tillman <dtillman@pivotal.io>
2016-07-05 11:50:34 -0600

    Updates for RTM

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+export STEELTOE_VERSION="1.0.0"
 export DOTNET_INSTALL_DIR="$PWD/.dotnetsdk"
 export PATH="$DOTNET_INSTALL_DIR:$PATH"
+# Patch project.json files
+cd ./scripts
+npm install
+node patch-project-json.js ../src/SteelToe.Discovery.Client/project.json $STEELTOE_VERSION-$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER $TRAVIS_TAG
+node patch-project-json.js ../src/SteelToe.Discovery.Eureka.Client/project.json $STEELTOE_VERSION-$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER $TRAVIS_TAG
+cd ..
 cd src
 dotnet restore
 cd ../test
