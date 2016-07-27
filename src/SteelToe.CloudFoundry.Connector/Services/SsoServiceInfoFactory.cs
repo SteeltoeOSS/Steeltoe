7fe476f24073af20bae33513c9cc412abb255554
Dave Tillman <dtillman@pivotal.io>
2016-07-27 14:02:33 -0600

    Add SSO Service Info

---
+++ src/SteelToe.CloudFoundry.Connector/Services/SsoServiceInfoFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SteelToe.Extensions.Configuration.CloudFoundry;
+
+namespace SteelToe.CloudFoundry.Connector.Services
+{
+    public class SsoServiceInfoFactory : ServiceInfoFactory
+    {
+
+        public SsoServiceInfoFactory() : base(new Tags("p-identity"), (string[]) null)
+        {
+        }
+   
+        public override IServiceInfo Create(Service binding)
+        {
+            string clientId = GetClientIdFromCredentials(binding.Credentials);
+            string clientSecret = GetClientSecretFromCredentials(binding.Credentials);
+            string authDomain = GetStringFromCredentials(binding.Credentials, "auth_domain");
+
+            return new SsoServiceInfo(binding.Name, clientId, clientSecret, authDomain );
+        }
+    
+    }
+}
---
+++ test/SteelToe.CloudFoundry.Connector.Test/Services/SsoServiceInfoTest.cs
@@ -0,0 +1,26 @@
+﻿using SteelToe.CloudFoundry.Connector.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SteelToe.CloudFoundry.Connector.Test.Services
+{
+    public class SsoServiceInfoTest
+    {
+        [Fact]
+        public void Constructor_CreatesExpected()
+        {
+            string clientId = "clientId";
+            string clientSecret = "clientSecret";
+            string authDomain = "https://p-spring-cloud-services.uaa.my-cf.com/oauth/token";
+            SsoServiceInfo r1 = new SsoServiceInfo("myId", clientId, clientSecret, authDomain);
+            Assert.Equal("myId", r1.Id);
+            Assert.Equal("clientId", r1.ClientId);
+            Assert.Equal("clientSecret", r1.ClientSecret);
+            Assert.Equal("https://p-spring-cloud-services.uaa.my-cf.com/oauth/token", r1.AuthDomain);
+
+        }
+    }
+}
