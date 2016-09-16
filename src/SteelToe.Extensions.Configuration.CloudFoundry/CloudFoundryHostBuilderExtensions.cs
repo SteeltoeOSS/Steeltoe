89010fe3f200b380e384992b738ef86c3d8318c3
mkreibe <mkreibe@ecsteam.com>
2016-09-16 12:49:16 -0600

    Added a sniffer for the PORT environment variable. This cleans up the need to do the following:
            public static void Main(string[] args)
            {
                var host = new WebHostBuilder()
                    .UseKestrel()
                    .UseUrls(GetServerUrls(args))
                    .UseContentRoot(Directory.GetCurrentDirectory())
                    .UseIISIntegration()
                    .UseStartup<Startup>()
                    .Build();
    
                host.Run();
            }
            private static string[] GetServerUrls(string[] args)
            {
                List<string> urls = new List<string>();
                for (int i = 0; i < args.Length; i++)
                {
                    if ("--server.urls".Equals(args[i], StringComparison.OrdinalIgnoreCase))
                    {
                        urls.Add(args[i + 1]);
                    }
                }
                return urls.ToArray();
            }
    
    ---------
    
     And simplifies it to just this:
    
            public static void Main(string[] args)
            {
                var host = new WebHostBuilder()
                    .UseKestrel()
                    .UseCloudFoundryHosting() // Added
                    .UseContentRoot(Directory.GetCurrentDirectory())
                    .UseIISIntegration()
                    .UseStartup<Startup>()
                    .Build();
    
                host.Run();
            }
    
    It also make it so that the manifest.yml files are not poluted with values like the following:
    
      command: ./MusicStoreUI --server.urls http://*:%PORT%
    
    and reduce it to:
    
      command: ./MusicStoreUI

---
+++ src/SteelToe.Extensions.Configuration.CloudFoundry/CloudFoundryHostBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace SteelToe.Extensions.Configuration.CloudFoundry
+{
+    public static class CloudFoundryHostBuilderExtensions
+    {
+        public static IWebHostBuilder UseCloudFoundryHosting(this IWebHostBuilder webHostBuilder)
+        {
+            if(webHostBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(webHostBuilder));
+            }
+
+            List<string> urls = new List<string>();
+
+            string portStr = Environment.GetEnvironmentVariable("PORT");
+            if (!string.IsNullOrWhiteSpace(portStr))
+            {
+                int port;
+                if (int.TryParse(portStr, out port))
+                {
+                    urls.Add($"http://*:{port}");
+                }
+            }
+
+            if (urls.Count > 0)
+            {
+                webHostBuilder.UseUrls(urls.ToArray());
+            }
+
+            return webHostBuilder;
+        }
+    }
+}
