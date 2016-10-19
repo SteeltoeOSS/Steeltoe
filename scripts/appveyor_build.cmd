25929b7a33e85a74f14290608a6ef37a36524eb7
Dave Tillman <dtillman@vmware.com>
2016-10-19 06:30:52 -0600

    Add DataProtection.Redis to scripts

--- scripts/appveyor_build.cmd
+++ scripts/appveyor_build.cmd
@@ -4,6 +4,7 @@
 cd %APPVEYOR_BUILD_FOLDER%\scripts
 call npm install
 call node patch-project-json.js ../src/Steeltoe.Security.Authentication.CloudFoundry/project.json %APPVEYOR_BUILD_VERSION% %APPVEYOR_REPO_TAG_NAME%
+call node patch-project-json.js ../src/Steeltoe.Security.DataProtection.Redis/project.json %APPVEYOR_BUILD_VERSION% %APPVEYOR_REPO_TAG_NAME%
 cd %APPVEYOR_BUILD_FOLDER%
 
 :: Restore packages
@@ -15,6 +16,9 @@ cd ..
 
 :: Build packages
 cd src\Steeltoe.Security.Authentication.CloudFoundry
+dotnet pack --configuration Release
+cd %APPVEYOR_BUILD_FOLDER%
+cd src\Steeltoe.Security.DataProtection.Redis
 dotnet pack --configuration Release
 cd %APPVEYOR_BUILD_FOLDER%
 
--- scripts/appveyor_tests.cmd
+++ scripts/appveyor_tests.cmd
@@ -4,3 +4,6 @@
 cd test\Steeltoe.Security.Authentication.CloudFoundry.Test
 dotnet test
 cd ..\..
+cd test\Steeltoe.Security.DataProtection.Redis.Test
+dotnet test
+cd ..\..
