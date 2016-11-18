92a8686b945f4412b1cea327e61cfaef4468adf8
Dave Tillman <dtillman@vmware.com>
2016-11-18 06:52:35 -0700

    More changes for .Net Core 1.1 update

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -3,11 +3,12 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update ; fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew install openssl ; fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/ ; fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/ ; fi
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then export DOTNET_SDK_URL=https://go.microsoft.com/fwlink/?LinkID=809128 ; fi
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then export DOTNET_SDK_URL=https://go.microsoft.com/fwlink/?LinkID=809129 ; fi   
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then export DOTNET_SDK_URL=https://go.microsoft.com/fwlink/?LinkID=834982 ; fi
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then export DOTNET_SDK_URL=https://go.microsoft.com/fwlink/?LinkID=834989 ; fi   
 export DOTNET_INSTALL_DIR="$PWD/.dotnetsdk"
 mkdir -p "$DOTNET_INSTALL_DIR"
 curl -L "$DOTNET_SDK_URL" | tar -xzv -C "$DOTNET_INSTALL_DIR"
 export PATH="$DOTNET_INSTALL_DIR:$PATH"
 dotnet --info
 npm install jsonfile -g
+
--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -3,9 +3,9 @@ export DOTNET_INSTALL_DIR="$PWD/.dotnetsdk"
 export PATH="$DOTNET_INSTALL_DIR:$PATH"
 # Run unit tests 
 cd test/Steeltoe.Extensions.Configuration.CloudFoundry.Test
-dotnet test --framework netcoreapp1.0
+dotnet test --framework netcoreapp1.1
 cd ../..
 cd test/Steeltoe.Extensions.Configuration.ConfigServer.Test
-dotnet test --framework netcoreapp1.0
+dotnet test --framework netcoreapp1.1
 cd ../..
 
