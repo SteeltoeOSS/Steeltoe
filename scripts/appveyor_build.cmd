38518b8f4c1e290b234b00fa578eae8002c7dc5e
Dave Tillman <dtillman@pivotal.io>
2017-04-20 05:18:25 -0600

    Add quotes around if statements

--- scripts/appveyor_build.cmd
+++ scripts/appveyor_build.cmd
@@ -6,10 +6,10 @@ dotnet restore --configfile nuget.config
 
 :: Build packages
 cd src\Steeltoe.Extensions.Configuration.CloudFoundry
-IF NOT %APPVEYOR_REPO_TAG_NAME%=="" (dotnet pack --configuration Release)
-IF %APPVEYOR_REPO_TAG_NAME%=="" (dotnet pack --configuration Release --version-suffix %STEELTOE_VERSION_SUFFIX%)
+IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release)
+IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release --version-suffix %STEELTOE_VERSION_SUFFIX%)
 cd %APPVEYOR_BUILD_FOLDER%
 cd src\Steeltoe.Extensions.Configuration.ConfigServer
-IF NOT %APPVEYOR_REPO_TAG_NAME%=="" (dotnet pack --configuration Release)
-IF %APPVEYOR_REPO_TAG_NAME%=="" (dotnet pack --configuration Release --version-suffix %STEELTOE_VERSION_SUFFIX%)
+IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release)
+IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release --version-suffix %STEELTOE_VERSION_SUFFIX%)
 cd %APPVEYOR_BUILD_FOLDER%
--- scripts/appveyor_install.cmd
+++ scripts/appveyor_install.cmd
@@ -5,6 +5,6 @@ dotnet --info
 SET number=00000%APPVEYOR_BUILD_NUMBER%
 SET STEELTOE_VERSION_SUFFIX=%APPVEYOR_REPO_BRANCH%-%number:~-5%
 echo %STEELTOE_VERSION_SUFFIX%
-IF %APPVEYOR_REPO_BRANCH%=="master" (copy config\nuget-master.config .\nuget.config)
-IF %APPVEYOR_REPO_BRANCH%=="dev" (copy config\nuget-dev.config .\nuget.config)
-IF NOT %APPVEYOR_REPO_TAG_NAME%=="" (copy config\nuget.config .\nuget.config)
+IF "%APPVEYOR_REPO_BRANCH%"=="master" COPY config\nuget-master.config .\nuget.config
+IF "%APPVEYOR_REPO_BRANCH%"=="dev" COPY config\nuget-dev.config .\nuget.config
+IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" COPY config\nuget.config .\nuget.config
