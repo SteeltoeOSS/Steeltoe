bdbb37282abb0ff48d23cb50a0802a263a6e2b5d
Dave Tillman <dtillman@pivotal.io>
2017-04-20 14:08:52 -0600

    Reorder eureka client build on linux/osx

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -3,13 +3,15 @@
 export STEELTOE_VERSION_SUFFIX=$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER
 echo $STEELTOE_VERSION_SUFFIX
 
-cd src/Steeltoe.Discovery.Client
-dotnet restore --configfile ../../nuget.config
-if [[ "$TRAVIS_TAG" != "" ]]; then dotnet build --framework netstandard1.3 --configuration Release ; fi
-if [[ "$TRAVIS_TAG" == "" ]]; then dotnet build --framework netstandard1.3 --configuration Release --version-suffix $STEELTOE_VERSION_SUFFIX ; fi
-cd ../..
 cd src/Steeltoe.Discovery.Eureka.Client
 dotnet restore --configfile ../../nuget.config
 if [[ "$TRAVIS_TAG" != "" ]]; then dotnet build --framework netstandard1.3 --configuration Release ; fi
 if [[ "$TRAVIS_TAG" == "" ]]; then dotnet build --framework netstandard1.3 --configuration Release --version-suffix $STEELTOE_VERSION_SUFFIX ; fi
 cd ../..
+
+cd src/Steeltoe.Discovery.Client
+dotnet restore --configfile ../../nuget.config
+if [[ "$TRAVIS_TAG" != "" ]]; then dotnet build --framework netstandard1.3 --configuration Release ; fi
+if [[ "$TRAVIS_TAG" == "" ]]; then dotnet build --framework netstandard1.3 --configuration Release --version-suffix $STEELTOE_VERSION_SUFFIX ; fi
+cd ../..
+
--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Run unit tests 
-cd test/Steeltoe.Discovery.Client.Test
+# Run unit tests
+cd test/Steeltoe.Discovery.Eureka.Client.Test
 dotnet restore --configfile ../../nuget.config
 dotnet test --framework netcoreapp1.1
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
 
-cd test/Steeltoe.Discovery.Eureka.Client.Test
+cd test/Steeltoe.Discovery.Client.Test
 dotnet restore --configfile ../../nuget.config
 dotnet test --framework netcoreapp1.1
 if [[ $? != 0 ]]; then exit 1 ; fi
