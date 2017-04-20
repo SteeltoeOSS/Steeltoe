3885ca8f9bb1765911f1548df6c43424ddff8d5a
Dave Tillman <dtillman@pivotal.io>
2017-04-20 04:55:31 -0600

    Add in nuget config files

--- scripts/appveyor_build.cmd
+++ scripts/appveyor_build.cmd
@@ -2,14 +2,14 @@
 
 :: Restore packages
 cd %APPVEYOR_BUILD_FOLDER%
-dotnet restore
+dotnet restore --configfile nuget.config
 
 :: Build packages
 cd src\Steeltoe.Extensions.Configuration.CloudFoundry
-IF %APPVEYOR_REPO_TAG%=="true" (dotnet pack --configuration Release)
-IF %APPVEYOR_REPO_TAG%=="false" (dotnet pack --configuration Release --version-suffix %STEELTOE_VERSION_SUFFIX%)
+IF NOT %APPVEYOR_REPO_TAG_NAME%=="" (dotnet pack --configuration Release)
+IF %APPVEYOR_REPO_TAG_NAME%=="" (dotnet pack --configuration Release --version-suffix %STEELTOE_VERSION_SUFFIX%)
 cd %APPVEYOR_BUILD_FOLDER%
 cd src\Steeltoe.Extensions.Configuration.ConfigServer
-IF %APPVEYOR_REPO_TAG%=="true" (dotnet pack --configuration Release)
-IF %APPVEYOR_REPO_TAG%=="false" (dotnet pack --configuration Release --version-suffix %STEELTOE_VERSION_SUFFIX%)
+IF NOT %APPVEYOR_REPO_TAG_NAME%=="" (dotnet pack --configuration Release)
+IF %APPVEYOR_REPO_TAG_NAME%=="" (dotnet pack --configuration Release --version-suffix %STEELTOE_VERSION_SUFFIX%)
 cd %APPVEYOR_BUILD_FOLDER%
