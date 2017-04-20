424583328219d367df70be83071d67725f1d21e3
Dave Tillman <dtillman@pivotal.io>
2017-04-20 11:47:30 -0600

    Try working around open file limit problem

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -3,15 +3,14 @@
 export STEELTOE_VERSION_SUFFIX=$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER
 echo $STEELTOE_VERSION_SUFFIX
 
-# Restore packages
-dotnet restore --configfile nuget.config
-
 # Build code
 cd src/Steeltoe.Extensions.Configuration.CloudFoundry
+dotnet restore --configfile ../../nuget.config
 if [[ "$TRAVIS_TAG" != "" ]]; then dotnet build --framework netstandard1.3 --configuration Release ; fi
 if [[ "$TRAVIS_TAG" == "" ]]; then dotnet build --framework netstandard1.3 --configuration Release --version-suffix $STEELTOE_VERSION_SUFFIX ; fi
 cd ../..
 cd src/Steeltoe.Extensions.Configuration.ConfigServer
+dotnet restore --configfile ../../nuget.config
 if [[ "$TRAVIS_TAG" != "" ]]; then dotnet build --framework netstandard1.3 --configuration Release ; fi
 if [[ "$TRAVIS_TAG" == "" ]]; then dotnet build --framework netstandard1.3 --configuration Release --version-suffix $STEELTOE_VERSION_SUFFIX ; fi
 cd ../..
--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -2,10 +2,12 @@
 
 # Run unit tests 
 cd test/Steeltoe.Extensions.Configuration.CloudFoundry.Test
+dotnet restore --configfile ../../nuget.config
 dotnet test --framework netcoreapp1.1
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
 cd test/Steeltoe.Extensions.Configuration.ConfigServer.Test
+dotnet restore --configfile ../../nuget.config
 dotnet test --framework netcoreapp1.1
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
