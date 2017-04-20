6630270a72e2395fc2d5604b7854e74b4a9e70f2
Dave Tillman <dtillman@pivotal.io>
2017-04-20 05:56:36 -0600

    Update travis build

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-export DOTNET_INSTALL_DIR="$PWD/.dotnetsdk"
-export PATH="$DOTNET_INSTALL_DIR:$PATH"
+
 # Run unit tests 
 cd test/Steeltoe.Extensions.Configuration.CloudFoundry.Test
 dotnet test --framework netcoreapp1.1
+if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
 cd test/Steeltoe.Extensions.Configuration.ConfigServer.Test
 dotnet test --framework netcoreapp1.1
+if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
 
