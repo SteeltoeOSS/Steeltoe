0f2fb38fb5d38ddfcf676178d05164ee43c3fb53
Dave Tillman <dtillman@pivotal.io>
2017-04-20 19:02:31 -0600

    Don't build code twice on OSX/Linux

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-export STEELTOE_VERSION_SUFFIX=$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER
-echo $STEELTOE_VERSION_SUFFIX
+echo Code is built in Unit Tests
 
-cd src/Steeltoe.Discovery.Eureka.Client
-dotnet restore --configfile ../../nuget.config
-if [[ "$TRAVIS_TAG" != "" ]]; then dotnet build --framework netstandard1.3 --configuration Release ; fi
-if [[ "$TRAVIS_TAG" == "" ]]; then dotnet build --framework netstandard1.3 --configuration Release --version-suffix $STEELTOE_VERSION_SUFFIX ; fi
-cd ../..
-
-cd src/Steeltoe.Discovery.Client
-dotnet restore --configfile ../../nuget.config
-if [[ "$TRAVIS_TAG" != "" ]]; then dotnet build --framework netstandard1.3 --configuration Release ; fi
-if [[ "$TRAVIS_TAG" == "" ]]; then dotnet build --framework netstandard1.3 --configuration Release --version-suffix $STEELTOE_VERSION_SUFFIX ; fi
-cd ../..
 
