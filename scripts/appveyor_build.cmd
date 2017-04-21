293dd870842f70e0b4f7a27f774926858b66e184
Dave Tillman <dtillman@pivotal.io>
2017-04-20 18:14:34 -0600

    Use localfeed for dependent P2P

--- scripts/appveyor_build.cmd
+++ scripts/appveyor_build.cmd
@@ -5,11 +5,15 @@ cd %APPVEYOR_BUILD_FOLDER%
 dotnet restore --configfile nuget.config
 
 :: Build packages
-cd src\Steeltoe.Discovery.Client
+cd src\Steeltoe.Discovery.Eureka.Client
+dotnet restore --configfile ..\..\nuget.config
 IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release)
 IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release --version-suffix %STEELTOE_VERSION_SUFFIX%)
+IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (nuget add -source %USERPROFILE%\localfeed bin\Release\Steeltoe.Discovery.Eureka.Client.%STEELTOE_VERSION%.nupkg)
+IF "%APPVEYOR_REPO_TAG_NAME%"=="" (nuget add -source %USERPROFILE%\localfeed bin\Release\Steeltoe.Discovery.Eureka.Client.%STEELTOE_VERSION%-%STEELTOE_VERSION_SUFFIX%.nupkg)
 cd %APPVEYOR_BUILD_FOLDER%
-cd src\Steeltoe.Discovery.Eureka.Client
+cd src\Steeltoe.Discovery.Client
+dotnet restore --configfile ..\..\nuget.config
 IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release)
 IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release --version-suffix %STEELTOE_VERSION_SUFFIX%)
 cd %APPVEYOR_BUILD_FOLDER%
