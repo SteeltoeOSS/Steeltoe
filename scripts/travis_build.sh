3f88dfae74b1c373ca8828af685dfe1cd274fab9
Dave Tillman <dtillman@pivotal.io>
2017-04-20 19:10:30 -0600

    Work around OSX file limit issue

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -2,4 +2,12 @@
 
 echo Code is built in Unit Tests
 
+cd src/Steeltoe.Discovery.Eureka.Client
+dotnet restore --configfile ../../nuget.config
+cd ../..
+
+cd src/Steeltoe.Discovery.Client
+dotnet restore --configfile ../../nuget.config
+cd ../..
+
 
