1ec7f33dc198f24c7cc574cc2a8c33a51f5839b8
Dave Tillman <dtillman@pivotal.io>
2017-04-21 05:45:52 -0600

    Polish .csproj changes

--- scripts/appveyor_build.cmd
+++ scripts/appveyor_build.cmd
@@ -7,7 +7,6 @@ IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release)
 IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release --version-suffix %STEELTOE_VERSION_SUFFIX%)
 IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (nuget add -source %USERPROFILE%\localfeed bin\Release\Steeltoe.CloudFoundry.Connector.%STEELTOE_VERSION%.nupkg)
 IF "%APPVEYOR_REPO_TAG_NAME%"=="" (nuget add -source %USERPROFILE%\localfeed bin\Release\Steeltoe.CloudFoundry.Connector.%STEELTOE_VERSION%-%STEELTOE_VERSION_SUFFIX%.nupkg)
-
 cd %APPVEYOR_BUILD_FOLDER%
 
 cd src\Steeltoe.CloudFoundry.Connector.MySql
