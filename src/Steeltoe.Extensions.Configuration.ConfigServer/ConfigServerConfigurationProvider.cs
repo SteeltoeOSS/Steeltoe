2762d0e12f3c4a5ee56787fe35964bcf5d2665d8
Dave Tillman <dtillman@pivotal.io>
2017-04-27 13:29:52 -0600

    Move back to net452 from net46

--- src/Steeltoe.Extensions.Configuration.ConfigServer/ConfigServerConfigurationProvider.cs
+++ src/Steeltoe.Extensions.Configuration.ConfigServer/ConfigServerConfigurationProvider.cs
@@ -279,7 +279,7 @@ namespace Steeltoe.Extensions.Configuration.ConfigServer
             // Get the request message 
             var request = GetRequestMessage(requestUri);
 
-#if NET46
+#if NET452
             // If certificate validation is disabled, inject a callback to handle properly
             RemoteCertificateValidationCallback prevValidator = null;
             if (!_settings.ValidateCertificates)
@@ -322,7 +322,7 @@ namespace Steeltoe.Extensions.Configuration.ConfigServer
                 _logger?.LogError("Config Server exception: {0}, path: {1}", e, requestUri);
                 throw;
             }
-#if NET46
+#if NET452
             finally
             {
                 ServicePointManager.ServerCertificateValidationCallback = prevValidator;
@@ -423,7 +423,7 @@ namespace Steeltoe.Extensions.Configuration.ConfigServer
         protected static HttpClient GetHttpClient(ConfigServerClientSettings settings)
         {
             HttpClient client = null;
-#if NET46
+#if NET452
             client = new HttpClient();
 #else
             if (settings != null && !settings.ValidateCertificates)
