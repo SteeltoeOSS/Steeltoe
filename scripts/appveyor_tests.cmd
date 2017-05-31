70e27cc8135f3b1c2e658e549e72ab4fbec56bb0
Dave Tillman <dtillman@pivotal.io>
2017-05-31 10:16:06 -0600

    Update to 1.1.0 and xunit update

--- scripts/appveyor_tests.cmd
+++ scripts/appveyor_tests.cmd
@@ -3,37 +3,37 @@
 :: Run unit tests 
 cd test\Steeltoe.CloudFoundry.Connector.Test
 dotnet restore --configfile ..\..\nuget.config
-dotnet test
+dotnet xunit -verbose
 if not "%errorlevel%"=="0" goto failure
 cd ..\..
 
 cd test\Steeltoe.CloudFoundry.Connector.MySql.Test
 dotnet restore --configfile ..\..\nuget.config
-dotnet test
+dotnet xunit -verbose
 if not "%errorlevel%"=="0" goto failure
 cd ..\..
 
 cd test\Steeltoe.CloudFoundry.Connector.Redis.Test
 dotnet restore --configfile ..\..\nuget.config
-dotnet test
+dotnet xunit -verbose
 if not "%errorlevel%"=="0" goto failure
 cd ..\..
 
 cd test\Steeltoe.CloudFoundry.Connector.PostgreSql.Test
 dotnet restore --configfile ..\..\nuget.config
-dotnet test
+dotnet xunit -verbose
 if not "%errorlevel%"=="0" goto failure
 cd ..\..
 
 cd test\Steeltoe.CloudFoundry.Connector.Rabbit.Test
 dotnet restore --configfile ..\..\nuget.config
-dotnet test
+dotnet xunit -verbose
 if not "%errorlevel%"=="0" goto failure
 cd ..\..
 
 cd test\Steeltoe.CloudFoundry.Connector.OAuth.Test
 dotnet restore --configfile ..\..\nuget.config
-dotnet test
+dotnet xunit -verbose
 if not "%errorlevel%"=="0" goto failure
 cd ..\..
 
