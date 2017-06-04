2457bc649914df7cc497b05628aa6b2665d40c68
Dave Tillman <dtillman@pivotal.io>
2017-06-04 10:49:20 -0600

    Updates to AddHystrixCommand()

--- src/Steeltoe.CircuitBreaker.Hystrix/HystrixServiceCollectionExtensions.cs
+++ src/Steeltoe.CircuitBreaker.Hystrix/HystrixServiceCollectionExtensions.cs
@@ -6,22 +6,47 @@ namespace Steeltoe.CircuitBreaker.Hystrix
 {
     public static class HystrixServiceCollectionExtensions
     {
-
-        public static void AddHystrixCommand<T>(this IServiceCollection services, IHystrixCommandGroupKey groupKey, IConfiguration config) where T : class
+        public static void AddHystrixCommand<TService, TImplementation>(this IServiceCollection services, IHystrixCommandGroupKey groupKey, IConfiguration config) 
+            where TService : class where TImplementation : class, TService
         {
             var strategy = HystrixPlugins.OptionsStrategy;
             var dynOpts = strategy.GetDynamicOptions(config);
 
-            var commandKey = HystrixCommandKeyDefault.AsKey(typeof(T).Name);
+            var commandKey = HystrixCommandKeyDefault.AsKey(typeof(TImplementation).Name);
 
             IHystrixCommandOptions opts = new HystrixCommandOptions(commandKey, null, dynOpts)
             {
                 GroupKey = groupKey
             };
-
-            services.AddTransient<T>((p) => (T)ActivatorUtilities.CreateInstance(p, typeof(T), opts));
+            services.AddTransient<TService, TImplementation>( (p) => (TImplementation)ActivatorUtilities.CreateInstance(p, typeof(TImplementation), opts));
         }
-        public static void AddHystrixCommand<T>(this IServiceCollection services, IHystrixCommandGroupKey groupKey, IHystrixCommandKey commandKey, IConfiguration config) where T : class
+        public static void AddHystrixCommand<TService, TImplementation>(this IServiceCollection services, IHystrixCommandGroupKey groupKey, IHystrixCommandKey commandKey, IConfiguration config)
+            where TService : class where TImplementation : class, TService
+        {
+            var strategy = HystrixPlugins.OptionsStrategy;
+            var dynOpts = strategy.GetDynamicOptions(config);
+
+            IHystrixCommandOptions opts = new HystrixCommandOptions(commandKey, null, dynOpts)
+            {
+                GroupKey = groupKey
+            };
+            services.AddTransient<TService, TImplementation>((p) => (TImplementation)ActivatorUtilities.CreateInstance(p, typeof(TImplementation), opts));
+        }
+        public static void AddHystrixCommand<TService>(this IServiceCollection services, IHystrixCommandGroupKey groupKey, IConfiguration config) where TService : class
+        {
+            var strategy = HystrixPlugins.OptionsStrategy;
+            var dynOpts = strategy.GetDynamicOptions(config);
+
+            var commandKey = HystrixCommandKeyDefault.AsKey(typeof(TService).Name);
+
+            IHystrixCommandOptions opts = new HystrixCommandOptions(commandKey, null, dynOpts)
+            {
+                GroupKey = groupKey
+            };
+
+            services.AddTransient<TService>((p) => (TService)ActivatorUtilities.CreateInstance(p, typeof(TService), opts));
+        }
+        public static void AddHystrixCommand<TService>(this IServiceCollection services, IHystrixCommandGroupKey groupKey, IHystrixCommandKey commandKey, IConfiguration config) where TService : class
         {
             var strategy = HystrixPlugins.OptionsStrategy;
             var dynOpts = strategy.GetDynamicOptions(config);
@@ -31,7 +56,7 @@ namespace Steeltoe.CircuitBreaker.Hystrix
                 GroupKey = groupKey
             };
 
-            services.AddTransient<T>((p) => (T)ActivatorUtilities.CreateInstance(p, typeof(T), opts));
+            services.AddTransient<TService>((p) => (TService)ActivatorUtilities.CreateInstance(p, typeof(TService), opts));
         }
     }
 }
