e058fb13eeee26a12d3a4ed854d620f1c1b7e8cc
Dave Tillman <dtillman@pivotal.io>
2017-06-08 06:15:22 -0600

    More fixes for symbols build

--- scripts/appveyor_build.cmd
+++ scripts/appveyor_build.cmd
@@ -3,10 +3,10 @@
 :: Build packages
 cd src\Steeltoe.CloudFoundry.Connector
 dotnet restore --configfile ..\..\nuget.config
-IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release)
-IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release --version-suffix %STEELTOE_VERSION_SUFFIX%)
-IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (nuget add -source %USERPROFILE%\localfeed bin\Release\Steeltoe.CloudFoundry.Connector.%STEELTOE_VERSION%.nupkg)
-IF "%APPVEYOR_REPO_TAG_NAME%"=="" (nuget add -source %USERPROFILE%\localfeed bin\Release\Steeltoe.CloudFoundry.Connector.%STEELTOE_VERSION%-%STEELTOE_VERSION_SUFFIX%.nupkg)
+IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE%)
+IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX% --include-symbols --include-source)
+IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (nuget add -source %USERPROFILE%\localfeed bin\%BUILD_TYPE%\Steeltoe.CloudFoundry.Connector.%STEELTOE_VERSION%.nupkg)
+IF "%APPVEYOR_REPO_TAG_NAME%"=="" (nuget add -source %USERPROFILE%\localfeed bin\%BUILD_TYPE%.\Steeltoe.CloudFoundry.Connector.%STEELTOE_VERSION%-%STEELTOE_VERSION_SUFFIX%.nupkg)
 cd %APPVEYOR_BUILD_FOLDER%
 
 cd src\Steeltoe.CloudFoundry.Connector.MySql
