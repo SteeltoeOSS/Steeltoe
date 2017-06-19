d895cc9d10114e9ff5d97d7a146e54a6668705e0
Dave Tillman <dtillman@pivotal.io>
2017-06-19 15:13:26 -0600

    Fix build for release candidates

--- scripts/appveyor_build.cmd
+++ scripts/appveyor_build.cmd
@@ -4,12 +4,24 @@
 :: Build packages
 cd src\Steeltoe.Extensions.Configuration.CloudFoundry
 dotnet restore --configfile ..\..\nuget.config
-IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE%)
+IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (
+    IF NOT "%STEELTOE_VERSION_SUFFIX%"=="" (
+        dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX%
+    ) ELSE (
+        dotnet pack --configuration %BUILD_TYPE%
+    )    
+)
 IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX% --include-symbols --include-source)
 cd %APPVEYOR_BUILD_FOLDER%
 
 cd src\Steeltoe.Extensions.Configuration.ConfigServer
 dotnet restore --configfile ..\..\nuget.config
-IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE%)
+IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (
+    IF NOT "%STEELTOE_VERSION_SUFFIX%"=="" (
+        dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX%
+    ) ELSE (
+        dotnet pack --configuration %BUILD_TYPE%
+    )    
+)
 IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX% --include-symbols --include-source)
 cd %APPVEYOR_BUILD_FOLDER%
