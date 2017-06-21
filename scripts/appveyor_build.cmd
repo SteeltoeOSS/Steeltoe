0f8f54591d269db63c5bd90983cc0f232796fd78
Dave Tillman <dtillman@pivotal.io>
2017-06-21 07:57:00 -0600

    Update build scripts

--- scripts/appveyor_build.cmd
+++ scripts/appveyor_build.cmd
@@ -12,7 +12,7 @@ IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (
     )    
 )
 IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX% --include-symbols --include-source)
-cd %APPVEYOR_BUILD_FOLDER%
+cd ..\..
 
 cd src\Steeltoe.Extensions.Configuration.ConfigServer
 dotnet restore --configfile ..\..\nuget.config
@@ -24,4 +24,4 @@ IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (
     )    
 )
 IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX% --include-symbols --include-source)
-cd %APPVEYOR_BUILD_FOLDER%
+cd ..\..
--- scripts/appveyor_install.cmd
+++ scripts/appveyor_install.cmd
@@ -2,14 +2,23 @@
 :: Output dotnet info
 dotnet --info
 
+SET CI_BUILD=%APPVEYOR%
 SET number=00000%APPVEYOR_BUILD_NUMBER%
 SET STEELTOE_VERSION=1.1.0
 SET STEELTOE_VERSION_SUFFIX=%APPVEYOR_REPO_BRANCH%-%number:~-5%
 IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" SET STEELTOE_VERSION_SUFFIX=%APPVEYOR_REPO_TAG_NAME:~6,5%
-SET STEELTOE_VERSION_SUFFIX=%STEELTOE_VERSION_SUFFIX: =% 
-echo %STEELTOE_VERSION_SUFFIX%
+IF NOT "%STEELTOE_VERSION_SUFFIX%"=="" (
+    SET STEELTOE_VERSION_SUFFIX=%STEELTOE_VERSION_SUFFIX: =%
+    SET STEELTOE_DASH_VERSION_SUFFIX=-%STEELTOE_VERSION_SUFFIX%
+) ELSE (
+    SET STEELTOE_DASH_VERSION_SUFFIX=
+)   
+echo "Version Suffix:" %STEELTOE_VERSION_SUFFIX%
 SET BUILD_TYPE=Release
 IF "%APPVEYOR_REPO_BRANCH%"=="master" COPY config\nuget-master.config .\nuget.config
 IF "%APPVEYOR_REPO_BRANCH%"=="dev" COPY config\nuget-dev.config .\nuget.config
 IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" COPY config\nuget.config .\nuget.config
+IF "%APPVEYOR_REPO_BRANCH%"=="master" COPY config\versions.props .\versions.props
+IF "%APPVEYOR_REPO_BRANCH%"=="dev" COPY config\versions.props .\versions.props
+IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" COPY config\versions.props .\versions.props
 IF "%APPVEYOR_REPO_BRANCH%"=="dev" SET BUILD_TYPE=Debug
