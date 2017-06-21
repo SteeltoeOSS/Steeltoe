5fb414cbcd50a978b60a3e5a6c3226cab19ae9cc
Dave Tillman <dtillman@pivotal.io>
2017-06-21 10:33:53 -0600

    Update scripts and project

--- scripts/appveyor_build.cmd
+++ scripts/appveyor_build.cmd
@@ -3,23 +3,52 @@
 :: Build packages
 cd src\Steeltoe.CircuitBreaker.Hystrix.Core
 dotnet restore --configfile ..\..\nuget.config
-IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE%)
+IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (
+    IF NOT "%STEELTOE_VERSION_SUFFIX%"=="" (
+        dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX%
+        nuget add "bin\%BUILD_TYPE%\Steeltoe.CircuitBreaker.Hystrix.Core.%STEELTOE_VERSION%-%STEELTOE_VERSION_SUFFIX%.nupkg" -Source "%USERPROFILE%\localfeed"
+    ) ELSE (
+        dotnet pack --configuration %BUILD_TYPE%
+        nuget add "bin\%BUILD_TYPE%\Steeltoe.CircuitBreaker.Hystrix.Core.%STEELTOE_VERSION%.nupkg" -Source "%USERPROFILE%\localfeed"
+    )    
+)
+
 IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX% --include-symbols --include-source)
-IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (nuget add -source %USERPROFILE%\localfeed bin\%BUILD_TYPE%\Steeltoe.CircuitBreaker.Hystrix.Core.%STEELTOE_VERSION%.nupkg)
-IF "%APPVEYOR_REPO_TAG_NAME%"=="" (nuget add -source %USERPROFILE%\localfeed bin\%BUILD_TYPE%\Steeltoe.CircuitBreaker.Hystrix.Core.%STEELTOE_VERSION%-%STEELTOE_VERSION_SUFFIX%.nupkg)
-cd %APPVEYOR_BUILD_FOLDER%
+IF "%APPVEYOR_REPO_TAG_NAME%"=="" (nuget add -Source %USERPROFILE%\localfeed bin\%BUILD_TYPE%\Steeltoe.CircuitBreaker.Hystrix.Core.%STEELTOE_VERSION%-%STEELTOE_VERSION_SUFFIX%.nupkg)
+cd ..\..
+
 cd src\Steeltoe.CircuitBreaker.Hystrix
 dotnet restore --configfile ..\..\nuget.config
-IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE%)
+IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (
+    IF NOT "%STEELTOE_VERSION_SUFFIX%"=="" (
+        dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX%
+    ) ELSE (
+        dotnet pack --configuration %BUILD_TYPE%
+    )    
+)
 IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX% --include-symbols --include-source)
-cd %APPVEYOR_BUILD_FOLDER%
+cd ..\..
+
 cd src\Steeltoe.CircuitBreaker.Hystrix.MetricsEvents
 dotnet restore --configfile ..\..\nuget.config
-IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE%)
+IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (
+    IF NOT "%STEELTOE_VERSION_SUFFIX%"=="" (
+        dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX%
+    ) ELSE (
+        dotnet pack --configuration %BUILD_TYPE%
+    )    
+)
 IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX% --include-symbols --include-source)
-cd %APPVEYOR_BUILD_FOLDER%
+cd ..\..
+
 cd src\Steeltoe.CircuitBreaker.Hystrix.MetricsStream
 dotnet restore --configfile ..\..\nuget.config
-IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE%)
+IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (
+    IF NOT "%STEELTOE_VERSION_SUFFIX%"=="" (
+        dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX%
+    ) ELSE (
+        dotnet pack --configuration %BUILD_TYPE%
+    )    
+)
 IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX% --include-symbols --include-source)
-cd %APPVEYOR_BUILD_FOLDER%
+cd ..\..
