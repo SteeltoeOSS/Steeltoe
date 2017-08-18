ba6beb8f937e69459f1336014c0d36e6d9d71656
Dave Tillman <dtillman@pivotal.io>
2017-08-18 15:31:36 -0600

    Update unit tests to Core 2.0, net461

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -3,13 +3,13 @@
 # Run unit tests 
 cd test/Steeltoe.Extensions.Configuration.CloudFoundry.Test
 dotnet restore --configfile ../../nuget.config
-dotnet xunit -verbose -framework netcoreapp1.1
+dotnet xunit -verbose -framework netcoreapp2.0
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
 
 cd test/Steeltoe.Extensions.Configuration.ConfigServer.Test
 dotnet restore --configfile ../../nuget.config
-dotnet xunit -verbose -framework netcoreapp1.1
+dotnet xunit -verbose -framework netcoreapp2.0
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
 
