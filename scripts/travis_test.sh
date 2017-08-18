d9b10606520f46e858c55cbc6203c5ed7343c57c
Dave Tillman <dtillman@pivotal.io>
2017-08-18 17:48:39 -0600

    Update unit tests to Core 2.0

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -3,12 +3,12 @@
 # Run unit tests
 cd test/Steeltoe.Discovery.Eureka.Client.Test
 dotnet restore --configfile ../../nuget.config
-dotnet xunit -verbose -framework netcoreapp1.1
+dotnet xunit -verbose -framework netcoreapp2.0
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
 
 cd test/Steeltoe.Discovery.Client.Test
 dotnet restore --configfile ../../nuget.config
-dotnet xunit -verbose -framework netcoreapp1.1
+dotnet xunit -verbose -framework netcoreapp2.0
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
