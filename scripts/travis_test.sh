df8ee6242f1cec16ecc72c45caebf0cb06b40a50
Dave Tillman <dtillman@pivotal.io>
2017-08-19 07:01:29 -0600

    Update to Core 2.0 on unit tests

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -3,36 +3,36 @@
 # Run unit tests
 cd test/Steeltoe.Management.Endpoint.Test
 dotnet restore --configfile ../../nuget.config
-dotnet xunit -verbose -framework netcoreapp1.1
+dotnet xunit -verbose -framework netcoreapp2.0
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
 cd test/Steeltoe.Management.Endpoint.CloudFoundry.Test
 dotnet restore --configfile ../../nuget.config
-dotnet xunit -verbose -framework netcoreapp1.1
+dotnet xunit -verbose -framework netcoreapp2.0
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
 cd test/Steeltoe.Management.Endpoint.Health.Test
 dotnet restore --configfile ../../nuget.config
-dotnet xunit -verbose -framework netcoreapp1.1
+dotnet xunit -verbose -framework netcoreapp2.0
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
 cd test/Steeltoe.Management.Endpoint.Info.Test
 dotnet restore --configfile ../../nuget.config
-dotnet xunit -verbose -framework netcoreapp1.1
+dotnet xunit -verbose -framework netcoreapp2.0
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
 cd test/Steeltoe.Management.Endpoint.Loggers.Test
 dotnet restore --configfile ../../nuget.config
-dotnet xunit -verbose -framework netcoreapp1.1
+dotnet xunit -verbose -framework netcoreapp2.0
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
 cd test/Steeltoe.Management.Endpoint.Trace.Test
 dotnet restore --configfile ../../nuget.config
-dotnet xunit -verbose -framework netcoreapp1.1
+dotnet xunit -verbose -framework netcoreapp2.0
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
 cd test/Steeltoe.Management.CloudFoundry.Test
 dotnet restore --configfile ../../nuget.config
-dotnet xunit -verbose -framework netcoreapp1.1
+dotnet xunit -verbose -framework netcoreapp2.0
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
