825938ddc962c7e40df0d2f92bab51e07fcfca90
Dave Tillman <dtillman@pivotal.io>
2017-08-19 06:23:42 -0600

    udpate unit tests to Core 2.0

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -3,6 +3,6 @@
 # Run unit tests
 cd test/Steeltoe.Extensions.Logging.CloudFoundry.Test
 dotnet restore --configfile ../../nuget.config
-dotnet xunit -verbose -framework netcoreapp1.1
+dotnet xunit -verbose -framework netcoreapp2.0
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
