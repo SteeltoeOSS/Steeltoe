e5f23a57005eb21acf91d0fdd80ea5c24109a7d4
Travis Illig <travis.illig@fiserv.com>
2017-08-30 10:27:44 -0700

    Configuration extensions for creating RedisServiceConnectorFactory. Includes split of app and connector config.

---
+++ src/Steeltoe.CloudFoundry.Connector.Redis/RedisCacheConfigurationExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Steeltoe.CloudFoundry.Connector.Services;
+
+namespace Steeltoe.CloudFoundry.Connector.Redis
+{
+    public static class RedisCacheConfigurationExtensions
+    {
+        public static RedisServiceConnectorFactory CreateRedisServiceConnectorFactory(this IConfiguration config, string serviceName = null)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            var redisConfig = new RedisCacheConnectorOptions(config);
+            return config.CreateRedisServiceConnectorFactory(config, serviceName);
+        }
+
+        public static RedisServiceConnectorFactory CreateRedisServiceConnectorFactory(this IConfiguration config, IConfiguration connectorConfiguration, string serviceName = null)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            if (connectorConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(connectorConfiguration));
+            }
+
+            var connectorOptions = new RedisCacheConnectorOptions(connectorConfiguration);
+            return config.CreateRedisServiceConnectorFactory(connectorOptions, serviceName);
+        }
+
+        public static RedisServiceConnectorFactory CreateRedisServiceConnectorFactory(this IConfiguration config, RedisCacheConnectorOptions connectorOptions, string serviceName = null)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            if (connectorOptions == null)
+            {
+                throw new ArgumentNullException(nameof(connectorOptions));
+            }
+
+            var info = serviceName == null ? config.GetSingletonServiceInfo<RedisServiceInfo>() : config.GetRequiredServiceInfo<RedisServiceInfo>(serviceName);
+            return new RedisServiceConnectorFactory(info, connectorOptions);
+        }
+    }
+}
