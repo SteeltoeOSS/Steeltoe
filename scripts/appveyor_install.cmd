3536a8eba00d481d89a88a24b3f248779522620d
Dave Tillman <dtillman@pivotal.io>
2017-09-22 08:57:36 -0600

    Changes to do update builds

--- scripts/appveyor_install.cmd
+++ scripts/appveyor_install.cmd
@@ -15,6 +15,8 @@ IF NOT "%STEELTOE_VERSION_SUFFIX%"=="" (
 )   
 echo "Version Suffix:" %STEELTOE_VERSION_SUFFIX%
 SET BUILD_TYPE=Release
+COPY config\nuget.config .\nuget.config
+COPY config\versions.props .\versions.props
 IF "%APPVEYOR_REPO_BRANCH%"=="master" COPY config\nuget-master.config .\nuget.config
 IF "%APPVEYOR_REPO_BRANCH%"=="dev" COPY config\nuget-dev.config .\nuget.config
 IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" COPY config\nuget.config .\nuget.config
--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -4,6 +4,8 @@ dotnet --info
 
 export CI_BUILD=True
 export STEELTOE_VERSION=1.1.1
+cp config/nuget.config ./nuget.config
+cp config/versions.props ./versions.props
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then cp config/nuget-master.config ./nuget.config ; fi
 if [[ "$TRAVIS_BRANCH" == "dev" ]]; then cp config/nuget-dev.config ./nuget.config ; fi
 if [[ "$TRAVIS_TAG" != "" ]]; then cp config/nuget.config ./nuget.config ; fi
