86cc88570bd80739cab38f879f7371fca97b95ab
Tim Hess <thess@pivotal.io>
2017-10-19 17:08:19 -0500

    Don't run EF6 tests in Travis as net461 isn't supported there

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -7,12 +7,6 @@ dotnet xunit -verbose -framework netcoreapp2.0
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
 
-cd test/Steeltoe.CloudFoundry.Connector.EF6.Test
-dotnet restore --configfile ../../nuget.config
-dotnet xunit -verbose -framework netcoreapp2.0
-if [[ $? != 0 ]]; then exit 1 ; fi
-cd ../..
-
 cd test/Steeltoe.CloudFoundry.Connector.EFCore.Test
 dotnet restore --configfile ../../nuget.config
 dotnet xunit -verbose -framework netcoreapp2.0
