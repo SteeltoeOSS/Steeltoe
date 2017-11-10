3240009f35841bfaa9e3784ee9d6f9db85715f01
Tim Hess <thess@pivotal.io>
2017-11-10 16:32:05 -0600

    Add HttpClient extensions for sending and receiving Json

---
+++ src/Steeltoe.Common.Http/HttpClientExtensions.cs
@@ -0,0 +1,54 @@
+﻿// Copyright 2017 the original author or authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Steeltoe.Common.Http
+{
+    public static class HttpClientExtensions
+    {
+        /// <summary>
+        /// Convert an object to Json and POST it
+        /// </summary>
+        /// <typeparam name="T">Type of object to serialize</typeparam>
+        /// <param name="httpClient">HttpClient doing the sending</param>
+        /// <param name="url">Url to POST to</param>
+        /// <param name="data">Object to send</param>
+        /// <returns>Task to be awaited</returns>
+        public static Task<HttpResponseMessage> PostAsJsonAsync<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonConvert.SerializeObject(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return httpClient.PostAsync(url, content);
+        }
+
+        public static Task<HttpResponseMessage> PutAsJsonAsync<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonConvert.SerializeObject(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return httpClient.PutAsync(url, content);
+        }
+
+        public static async Task<T> ReadAsJsonAsync<T>(this HttpContent content)
+        {
+            var dataAsString = await content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(dataAsString);
+        }
+    }
+}
---
+++ test/Steeltoe.Common.Http.Test/HttpClientExtensionsTest.cs
@@ -0,0 +1,59 @@
+﻿// Copyright 2017 the original author or authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using RichardSzalay.MockHttp;
+using System.Net.Http;
+using Xunit;
+
+namespace Steeltoe.Common.Http.Test
+{
+    public class HttpClientExtensionsTest
+    {
+        [Fact]
+        public async void PostAsJsonAsync_Issues_POST_WithJSON()
+        {
+            // arrange
+            var mockHttpMessageHandler = new MockHttpMessageHandler();
+            var authRequest = mockHttpMessageHandler.Expect(HttpMethod.Post, "http://test-server/*").Respond("application/json", "{\"Name\": \"Test\", \"Value\": \"Poster\"}");
+            var client = new HttpClient(mockHttpMessageHandler);
+
+            // act
+            var result = await client.PostAsJsonAsync("http://test-server/", new SerializationTestObject { Name = "Sample", Value = "Test" });
+            var responseMessage = await result.Content.ReadAsJsonAsync<SerializationTestObject>();
+
+            // assert
+            mockHttpMessageHandler.VerifyNoOutstandingExpectation();
+            Assert.Equal("Test", responseMessage.Name);
+            Assert.Equal("Poster", responseMessage.Value);
+        }
+
+        [Fact]
+        public async void PutAsJsonAsync_Issues_PUT_WithJSON()
+        {
+            // arrange
+            var mockHttpMessageHandler = new MockHttpMessageHandler();
+            var authRequest = mockHttpMessageHandler.Expect(HttpMethod.Put, "http://test-server/*").Respond("application/json", "{\"Name\": \"Test\", \"Value\": \"PutPut\"}");
+            var client = new HttpClient(mockHttpMessageHandler);
+
+            // act
+            var result = await client.PutAsJsonAsync("http://test-server/", new SerializationTestObject { Name = "Sample", Value = "Test" });
+            var responseMessage = await result.Content.ReadAsJsonAsync<SerializationTestObject>();
+
+            // assert
+            mockHttpMessageHandler.VerifyNoOutstandingExpectation();
+            Assert.Equal("Test", responseMessage.Name);
+            Assert.Equal("PutPut", responseMessage.Value);
+        }
+    }
+}
