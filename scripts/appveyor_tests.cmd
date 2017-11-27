68b4c5e387204dbb32acea16ec0f26251ad37f73
Tim Hess <thess@pivotal.io>
2017-11-27 16:55:03 -0600

    verbosity switch on dotnet test is not the same as on dotnet xunit

--- scripts/appveyor_tests.cmd
+++ scripts/appveyor_tests.cmd
@@ -3,37 +3,37 @@
 :: Run unit tests 
 cd test\Steeltoe.Extensions.Configuration.CloudFoundry.Test
 dotnet restore --configfile ..\..\nuget.config
-dotnet test -verbose
+dotnet test --verbosity n
 if not "%errorlevel%"=="0" goto failure
 cd ..\..
 
 cd test\Steeltoe.Extensions.Configuration.CloudFoundryCore.Test
 dotnet restore --configfile ..\..\nuget.config
-dotnet test -verbose
+dotnet test --verbosity n
 if not "%errorlevel%"=="0" goto failure
 cd ..\..
 
 cd test\Steeltoe.Extensions.Configuration.CloudFoundryAutofac.Test
 dotnet restore --configfile ..\..\nuget.config
-dotnet test -verbose
+dotnet test --verbosity n
 if not "%errorlevel%"=="0" goto failure
 cd ..\..
 
 cd test\Steeltoe.Extensions.Configuration.ConfigServer.Test
 dotnet restore --configfile ..\..\nuget.config
-dotnet test -verbose
+dotnet test --verbosity n
 if not "%errorlevel%"=="0" goto failure
 cd ..\..
 
 cd test\Steeltoe.Extensions.Configuration.ConfigServerCore.Test
 dotnet restore --configfile ..\..\nuget.config
-dotnet test -verbose
+dotnet test --verbosity n
 if not "%errorlevel%"=="0" goto failure
 cd ..\..
 
 cd test\Steeltoe.Extensions.Configuration.ConfigServerAutofac.Test
 dotnet restore --configfile ..\..\nuget.config
-dotnet test -verbose
+dotnet test --verbosity n
 if not "%errorlevel%"=="0" goto failure
 cd ..\..
 
