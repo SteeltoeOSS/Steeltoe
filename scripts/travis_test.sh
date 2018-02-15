621c240a719f1f298f3c27619500b31be81d3eed
Tim Hess <thess@pivotal.io>
2018-02-15 10:18:03 -0600

    Don't run Autofac tests on Travis

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -18,9 +18,3 @@ dotnet restore --configfile ../../nuget.config
 dotnet xunit -verbose -framework netcoreapp2.0
 if [[ $? != 0 ]]; then exit 1 ; fi
 cd ../..
-
-cd test/Steeltoe.CloudFoundry.ConnectorAutofac.Test
-dotnet restore --configfile ../../nuget.config
-dotnet xunit -verbose -framework netcoreapp2.0
-if [[ $? != 0 ]]; then exit 1 ; fi
-cd ../..
