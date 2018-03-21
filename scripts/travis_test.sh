dec17ffe98ca83e451c3ff18bb44f7e216692b88
Tim Hess <thess@pivotal.io>
2018-03-21 17:08:30 -0500

    Use the new build process [#153640396]

--- scripts/travis_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run unit tests
-cd test/Steeltoe.Extensions.Logging.DynamicLogger.Test
-dotnet restore --configfile ../../nuget.config
-dotnet xunit -verbose -framework netcoreapp2.0
-if [[ $? != 0 ]]; then exit 1 ; fi
-cd ../..
