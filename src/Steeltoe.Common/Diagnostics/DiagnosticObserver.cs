a435765b8f50835e4aad12a481bb0c53e766a33c
Dave Tillman <dtillman@pivotal.io>
2018-04-06 09:13:49 -0600

    Initial add of common Diagnostics code

---
+++ src/Steeltoe.Common/Diagnostics/DiagnosticObserver.cs
@@ -0,0 +1,97 @@
+﻿// Copyright 2017 the original author or authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Steeltoe.Common.Diagnostics
+{
+    public abstract class DiagnosticObserver : IDiagnosticObserver
+    {
+        public string ListenerName { get; }
+
+        public string ObserverName { get; }
+
+        protected ILogger Logger { get; }
+
+        protected IDisposable Subscription { get; set; }
+
+        protected DiagnosticObserver(string name, string listenerName,  ILogger logger = null)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException(nameof(name));
+            }
+
+            if (string.IsNullOrEmpty(listenerName))
+            {
+                throw new ArgumentException(nameof(listenerName));
+            }
+
+            ObserverName = name;
+            ListenerName = listenerName;
+            Logger = logger;
+        }
+
+        public void Dispose()
+        {
+            if (Subscription != null)
+            {
+                Subscription.Dispose();
+            }
+
+            Subscription = null;
+            Logger?.LogInformation("DiagnosticObserver {observer} Disposed", ObserverName);
+        }
+
+        public void Subscribe(DiagnosticListener listener)
+        {
+            if (ListenerName == listener.Name)
+            {
+                if (Subscription != null)
+                {
+                    Dispose();
+                }
+
+                Subscription = listener.Subscribe(this);
+                Logger?.LogInformation("DiagnosticObserver {observer} Subscribed to {listener}", ObserverName, listener.Name);
+            }
+        }
+
+        public virtual void OnCompleted()
+        {
+        }
+
+        public virtual void OnError(Exception error)
+        {
+        }
+
+        public virtual void OnNext(KeyValuePair<string, object> @event)
+        {
+            try
+            {
+                ProcessEvent(@event.Key, @event.Value);
+            }
+            catch (Exception e)
+            {
+                Logger?.LogError(e, "ProcessEvent exception: {Id}", @event.Key);
+            }
+        }
+
+        public abstract void ProcessEvent(string @event, object arg);
+    }
+}
---
+++ src/Steeltoe.Common/Diagnostics/DiagnosticsManager.cs
@@ -0,0 +1,113 @@
+﻿// Copyright 2017 the original author or authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace Steeltoe.Common.Diagnostics
+{
+    public class DiagnosticsManager : IObserver<DiagnosticListener>, IDisposable, IDiagnosticsManager
+    {
+        private const int POLL_DELAY_MILLI = 15000;
+
+        private IDisposable listenersSubscription;
+        private ILogger<DiagnosticsManager> logger;
+        private IList<IDiagnosticObserver> observers;
+        private IList<IPolledDiagnosticSource> sources;
+        private Thread workerThread;
+        private bool workerThradShutdown = false;
+
+        public DiagnosticsManager(IEnumerable<IPolledDiagnosticSource> polledSources, IEnumerable<IDiagnosticObserver> observers, ILogger<DiagnosticsManager> logger = null)
+        {
+            if (observers == null)
+            {
+                throw new ArgumentNullException(nameof(observers));
+            }
+
+            this.logger = logger;
+            this.observers = observers.ToList();
+            this.sources = polledSources.ToList();
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+
+        public void OnCompleted()
+        {
+        }
+
+        public void OnError(Exception error)
+        {
+        }
+
+        public void OnNext(DiagnosticListener value)
+        {
+            foreach (var listener in observers)
+            {
+                listener.Subscribe(value);
+            }
+        }
+
+        public void Start()
+        {
+            this.listenersSubscription = DiagnosticListener.AllListeners.Subscribe(this);
+
+            workerThread = new Thread(this.Poller)
+            {
+                IsBackground = true,
+                Name = "DiagnosticsPoller"
+            };
+            workerThread.Start();
+        }
+
+        public void Stop()
+        {
+            workerThradShutdown = true;
+
+            foreach (var listener in observers)
+            {
+                listener.Dispose();
+            }
+
+            observers.Clear();
+        }
+
+        private void Poller(object obj)
+        {
+            while (!workerThradShutdown)
+            {
+                try
+                {
+                    foreach (var source in this.sources)
+                    {
+                        source.Poll();
+                    }
+
+                    Thread.Sleep(POLL_DELAY_MILLI);
+                }
+                catch (Exception e)
+                {
+                    logger?.LogError(e, "Metrics poller exception, terminating");
+                    return;
+                }
+            }
+        }
+    }
+}
