7812e01ca0555446b0a7071b66922b5aa4e7b192
Andrew Stakhov <andrew.stakhov@gmail.com>
2019-03-27 07:56:54 -0400

    Runner for launching Cloud Foundry tasks bundled with the app (#58)
    * Runner for launching Cloud Foundry tasks bundled with the app
    
    * Whitespace fixes & use https for apache license

---
+++ src/Steeltoe.Management.CloudFoundryTasks/DelegatingTask.cs
@@ -0,0 +1,33 @@
+// Copyright 2017 the original author or authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Steeltoe.Common.Tasks;
+
+namespace Steeltoe.Management.CloudFoundryTasks
+{
+    public class DelegatingTask : IApplicationTask
+    {
+        private readonly Action _run;
+
+        public DelegatingTask(string name, Action run)
+        {
+            _run = run;
+            Name = name;
+        }
+
+        public string Name { get; }
+        public void Run() => _run();
+    }
+}
