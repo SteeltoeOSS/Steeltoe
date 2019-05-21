bab9460f3d10cedf8201656a0b7f01f9d9090530
Jesper Liljegren <jesper.liljegren@gmail.com>
2019-05-21 07:16:31 +0200

    Extension-method to mask basic authentication details if they exist on an Uri() Useful for logging purposes

---
+++ src/Common/src/Common/Extensions/UriExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Steeltoe.Common.Extensions
+{
+    public static class UriExtensions
+    {
+        public static string ToMaskedString(this Uri source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (string.IsNullOrEmpty(source.UserInfo))
+            {
+                return source.ToString();
+            }
+
+            var builder = new UriBuilder(source)
+            {
+                UserName = "****",
+                Password = "****"
+            };
+
+            return builder.Uri.ToString();
+        }
+    }
+}
---
+++ src/Common/test/Common.Test/Extensions/UriExtensionsTest.cs
@@ -0,0 +1,37 @@
+﻿using Steeltoe.Common.Extensions;
+using System;
+using Xunit;
+
+namespace Steeltoe.Common.Test.Extensions
+{
+    public class UriExtensionsTest
+    {
+        [Fact]
+        public void MaskExistingBasicAuthentication()
+        {
+            // Arrange
+            var uri = new Uri("http://username:password@www.example.com/");
+            var expected = "http://****:****@www.example.com/";
+
+            // Act
+            var masked = uri.ToMaskedString();
+
+            // Assert
+            Assert.Equal(expected, masked);
+        }
+
+        [Fact]
+        public void DontMaskIfNotBasicAuthenticationExists()
+        {
+            // Arrange
+            var uri = new Uri("http://www.example.com/");
+            var expected = uri.ToString();
+
+            // Act
+            var masked = uri.ToMaskedString();
+
+            // Assert
+            Assert.Equal(expected, masked);
+        }
+    }
+}
