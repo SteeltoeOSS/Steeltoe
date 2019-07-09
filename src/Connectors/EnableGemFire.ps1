e39139cbd899d83eaf19229864e894024e441711
Tim Hess <thess@pivotal.io>
2019-07-09 07:18:49 -0500

    get GemFire dll on more platforms

--- src/Connectors/EnableGemFire.ps1
+++ src/Connectors/EnableGemFire.ps1
@@ -4,13 +4,15 @@ Param(
     [string]$PivNetAPIToken
  )
 
-if (-Not (Get-Command "pivnet.exe" -ErrorAction SilentlyContinue))
+ Set-Location $PSScriptRoot
+ 
+if (-Not (Get-Command "pivnet" -ErrorAction SilentlyContinue))
 {
     Write-Host "Downloading PivNet client..."
     # Download pivnet cli ... TODO: get latest instead of hardcoded version
-    (New-Object System.Net.WebClient).DownloadFile("https://github.com/pivotal-cf/pivnet-cli/releases/download/v0.0.60/pivnet-windows-amd64-0.0.60", "$PSScriptRoot\pivnet.exe")  
-    Write-Host "Adding alias 'pivnet' for $PSScriptRoot\pivnet.exe"
-    Set-Alias -Name pivnet -Value "$PSScriptRoot\pivnet.exe"
+    (New-Object System.Net.WebClient).DownloadFile("https://github.com/pivotal-cf/pivnet-cli/releases/download/v0.0.60/pivnet-windows-amd64-0.0.60", "$PSScriptRoot/pivnet.exe")  
+    Write-Host "Adding alias 'pivnet' for .\pivnet.exe"
+    Set-Alias -Name pivnet -Value ".\pivnet.exe"
 }
 
 # login with API token (deprecated!)
@@ -29,15 +31,21 @@ $fileName = (($productFile | jq '.aws_object_key') -split "/")[2] -replace '"', 
 Write-Host "File Name to download: $fileName"
 
 # Download archive
-pivnet download-product-files -p pivotal-gemfire -r $gemfireReleaseVersion -i $fileId -d $PSScriptRoot
+if (-Not (Test-Path $fileName))
+{
+    Write-Host "Downloading $fileName"
+    pivnet download-product-files -p pivotal-gemfire -r $gemfireReleaseVersion -i $fileId
+}
 
-if (Get-Command "7z.exe" -ErrorAction SilentlyContinue)
+if (Get-Command "7z" -ErrorAction SilentlyContinue)
 {
     # Unzip file
-    Write-Host "Use 7zip to extract Pivotal.GemFire.dll from $fileName to $PSScriptRoot"
-    7z e "$PSScriptRoot\$fileName" pivotal-gemfire-native\bin\Pivotal.GemFire.dll -r -o"$PSScriptRoot"
+    $dllLocation = Join-Path (Join-Path "pivotal-gemfire-native" "bin") "Pivotal.GemFire.dll"
+    Write-Host "Use 7zip to extract $dllLocation from $fileName"
+    Write-Host "7z e $fileName $dllLocation -r"
+    7z e $fileName $dllLocation -r
 }
 else
 {
-    Write-Host "7zip not found, manually extract Pivotal.GemFire.dll from $fileName to $PSScriptRoot to continue!"
+    Write-Host "7zip not found, manually extract Pivotal.GemFire.dll from $fileName to continue!"
 }
