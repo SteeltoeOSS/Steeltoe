d9177f972bb9dcdf5aba0a6f7145eaace83f3aec
Tim Hess <thess@pivotal.io>
2019-11-06 18:36:42 -0500

    Add HostBuilder extensions for easier dynamic logging #122 (#158)
    * Add HostBuilder extensions for easier dynamic logging #122
    don't pack TestResources

---
+++ src/Logging/src/DynamicLogger/DynamicLoggerHostBuilderExtensions.cs
@@ -0,0 +1,53 @@
+﻿// Copyright 2017 the original author or authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.Console;
+using System.Linq;
+
+namespace Steeltoe.Extensions.Logging.DynamicLogger
+{
+    public static class DynamicLoggerHostBuilderExtensions
+    {
+        /// <summary>
+        /// Adds Dynamic Console Logging to your application. Removes ConsoleLoggerProvider if found (to prevent duplicate console log entries)<para />
+        /// Also calls ILoggingBuilder.AddConfiguration() if not previously called.
+        /// </summary>
+        /// <param name="hostBuilder">Your HostBuilder</param>
+        public static IHostBuilder AddDynamicLogging(this IHostBuilder hostBuilder)
+        {
+            return hostBuilder
+                .ConfigureLogging(ilb =>
+                {
+                    // remove the original ConsoleLoggerProvider to prevent duplicate logging
+                    var serviceDescriptor = ilb.Services.FirstOrDefault(descriptor => descriptor.ImplementationType == typeof(ConsoleLoggerProvider));
+                    if (serviceDescriptor != null)
+                    {
+                        ilb.Services.Remove(serviceDescriptor);
+                    }
+
+                    // make sure logger provider configurations are available
+                    if (!ilb.Services.Any(descriptor => descriptor.ServiceType == typeof(ILoggerProviderConfiguration<ConsoleLoggerProvider>)))
+                    {
+                        ilb.AddConfiguration();
+                    }
+
+                    ilb.AddDynamicConsole();
+                });
+        }
+    }
+}
