de3ade3f4f1cd7f37fe59d24bf5e826f303150b8
Tim Hess <thess@pivotal.io>
2019-11-08 13:07:44 -0500

    Add HostBuilder extension for ServiceDiscovery  (#159)
    * provides configuration of ServiceDiscovery via a single line of code #156
    * use different file names between tests for temp files to address intermittently CI failure

---
+++ src/Discovery/src/ClientCore/DiscoveryClientStartupFilter.cs
@@ -0,0 +1,36 @@
+﻿// Copyright 2017 the original author or authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace Steeltoe.Discovery.Client
+{
+    /// <summary>
+    /// Provides an alternate means of activating the Discovery client. Use this filter in place of calling UseDiscoveryClient in Startup.Configure().
+    /// </summary>
+    public class DiscoveryClientStartupFilter : IStartupFilter
+    {
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return app =>
+            {
+                 next(app);
+
+                 app.UseDiscoveryClient();
+            };
+        }
+    }
+}
