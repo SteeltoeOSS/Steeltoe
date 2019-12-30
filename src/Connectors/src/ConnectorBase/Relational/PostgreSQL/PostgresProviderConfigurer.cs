e4c84f7c87efbb1ced97f0578d4ff1a06933340d
Tim Hess <thess@pivotal.io>
2019-12-30 12:47:24 -0600

    Provide access to Postgres cert-related properties, including SslMode.Requre #199

--- src/Connectors/src/ConnectorBase/Relational/PostgreSQL/PostgresProviderConfigurer.cs
+++ src/Connectors/src/ConnectorBase/Relational/PostgreSQL/PostgresProviderConfigurer.cs
@@ -34,7 +34,11 @@ namespace Steeltoe.Connector.PostgreSql
 
             if (!string.IsNullOrEmpty(si.Uri))
             {
-                configuration.Port = si.Port;
+                if (si.Port > 0)
+                {
+                    configuration.Port = si.Port;
+                }
+
                 if (configuration.UrlEncodedCredentials)
                 {
                     configuration.Username = WebUtility.UrlDecode(si.UserName);
@@ -48,7 +52,48 @@ namespace Steeltoe.Connector.PostgreSql
 
                 configuration.Host = si.Host;
                 configuration.Database = si.Path;
+                if (si.Query != null)
+                {
+                    foreach (var kvp in UriExtensions.ParseQuerystring(si.Query))
+                    {
+                        if (kvp.Key.Equals("sslmode", StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            // Npgsql parses SSL Mode into an enum, the first character must be capitalized
+                            configuration.SslMode = FirstCharToUpper(kvp.Value);
+                        }
+                        else if (kvp.Key.Equals("sslcert"))
+                        {
+                            // TODO: Map this client cert into the npgsql client cert callback
+                            configuration.ClientCertificate = kvp.Value;
+                        }
+                        else if (kvp.Key.Equals("sslkey"))
+                        {
+                            // TODO: Map this client cert into the npgsql client cert callback
+                            configuration.ClientKey = kvp.Value;
+                        }
+                        else if (kvp.Key.Equals("sslrootcert"))
+                        {
+                            // TODO: Map this client cert into the npgsql remote cert validation callback
+                            configuration.SslRootCertificate = kvp.Value;
+                        }
+                        else
+                        {
+                            configuration.Options.Add(kvp.Key, kvp.Value);
+                        }
+                    }
+                }
             }
         }
+
+        // from https://stackoverflow.com/a/4405876/761468
+        private string FirstCharToUpper(string input) =>
+            input switch
+            {
+                null => throw new ArgumentNullException(nameof(input)),
+#pragma warning disable SA1122 // Use string.Empty for empty strings
+                "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+#pragma warning restore SA1122 // Use string.Empty for empty strings
+                _ => input.First().ToString().ToUpper() + input.Substring(1)
+            };
     }
 }
