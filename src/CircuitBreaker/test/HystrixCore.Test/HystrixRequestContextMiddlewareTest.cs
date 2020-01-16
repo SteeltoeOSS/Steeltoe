eb93cbd4a5c6f592acbc116041f8773374805da3
Dave Tillman <dtillman@pivotal.io>
2020-01-16 10:25:51 -0700

    Shutdown Hystrix metrics threads [#189] (#213)
    * Shutdown Hystrix metrics threads [#189]
    
    * Update src/CircuitBreaker/src/HystrixCore/Steeltoe.CircuitBreaker.HystrixCore.csproj
    
    Co-Authored-By: Tim Hess <thess@pivotal.io>
    
    Co-authored-by: Tim Hess <thess@pivotal.io>

--- src/CircuitBreaker/test/HystrixCore.Test/HystrixRequestContextMiddlewareTest.cs
+++ src/CircuitBreaker/test/HystrixCore.Test/HystrixRequestContextMiddlewareTest.cs
@@ -13,7 +13,9 @@
 // limitations under the License.
 
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
 using Steeltoe.CircuitBreaker.Hystrix.Strategy.Concurrency;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -29,10 +31,53 @@ namespace Steeltoe.CircuitBreaker.Hystrix.Test
                 Assert.True(HystrixRequestContext.IsCurrentThreadInitialized);
                 return Task.FromResult<int>(1);
             };
-            var reqContext = new HystrixRequestContextMiddleware(del);
+            var life = new TestLifecyecle();
+            var reqContext = new HystrixRequestContextMiddleware(del, life);
             HttpContext context = new DefaultHttpContext();
             await reqContext.Invoke(context);
             Assert.False(HystrixRequestContext.IsCurrentThreadInitialized);
+            life.StopApplication();
+        }
+
+        [Fact]
+        public void HystrixRequestContextMiddleware_RegistersStoppingAction()
+        {
+            RequestDelegate del = (ctx) =>
+            {
+                Assert.True(HystrixRequestContext.IsCurrentThreadInitialized);
+                return Task.FromResult<int>(1);
+            };
+            var life = new TestLifecyecle();
+            var reqContext = new HystrixRequestContextMiddleware(del, life);
+            Assert.True(life.Registered);
+            life.StopApplication();
+        }
+
+#pragma warning disable CS0618 // Type or member is obsolete
+        private class TestLifecyecle : IApplicationLifetime
+#pragma warning restore CS0618 // Type or member is obsolete
+        {
+            public bool Registered = false;
+
+            private readonly CancellationTokenSource _stoppingSource = new CancellationTokenSource();
+
+            public CancellationToken ApplicationStarted => throw new System.NotImplementedException();
+
+            public CancellationToken ApplicationStopping
+            {
+                get
+                {
+                    Registered = true;
+                    return _stoppingSource.Token;
+                }
+            }
+
+            public CancellationToken ApplicationStopped => throw new System.NotImplementedException();
+
+            public void StopApplication()
+            {
+                _stoppingSource.Cancel();
+            }
         }
     }
 }
