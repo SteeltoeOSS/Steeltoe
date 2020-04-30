d7843ea0cfdc9ca545afc4c61c2c86df824c3f0c
Hananiel Sarella <hsarella@pivotal.io>
2020-04-30 14:16:26 -0400

    Add support for registering to SpringBootAdminServer (#306)
    * EventCounter WIP
    
    * Add event counter metrics
    
    * Cherry pick d5b0251 - content header handling
    
    * Support registration to SpringBootAdminClient
    
    * Code review comments
    
    * Apply suggestions from code review
    
    Co-Authored-By: Tim Hess <thess@pivotal.io>
    
    * Fix unit test
    
    * Use helper to get client
    
    Co-authored-by: Tim Hess <thess@pivotal.io>

---
+++ src/Management/src/EndpointCore/SpringBootAdminClient/BootAdminAppBuilderExtensions.cs
@@ -0,0 +1,93 @@
+﻿// Copyright 2017 the original author or authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Steeltoe.Common;
+using Steeltoe.Common.Http;
+using Steeltoe.Management.Endpoint.Health;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Steeltoe.Management.Endpoint.SpringBootAdminClient
+{
+    public static class BootAdminAppBuilderExtensions
+    {
+        private const int ConnectionTimeoutMs = 100000;
+        private static RegistrationResult registrationResult;
+
+        internal static RegistrationResult RegistrationResult { get => registrationResult; }
+
+        public static void RegisterSpringBootAdmin(this IApplicationBuilder builder, IConfiguration configuration, HttpClient testClient = null)
+        {
+            if (builder is null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (configuration is null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            var appInfo = builder.ApplicationServices.GetApplicationInstanceInfo();
+            var options = new BootAdminClientOptions(configuration, appInfo);
+            var mgmtOptions = new ManagementEndpointOptions(configuration);
+            var healthOptions = new HealthEndpointOptions(configuration);
+            var basePath = options.BasePath;
+
+            var app = new Application()
+            {
+                Name = options.ApplicationName ?? "Steeltoe",
+                HealthUrl = new Uri($"{basePath}{mgmtOptions.Path}/{healthOptions.Path}"),
+                ManagementUrl = new Uri($"{basePath}{mgmtOptions.Path}"),
+                ServiceUrl = new Uri($"{basePath}/"),
+                Metadata = new Metadata() { Startup = DateTime.Now }
+            };
+            var lifetime = builder.ApplicationServices.GetService<IHostApplicationLifetime>();
+            lifetime.ApplicationStarted.Register(() =>
+            {
+                var httpClient = testClient ?? HttpClientHelper.GetHttpClient(false, ConnectionTimeoutMs);
+
+                var content = JsonConvert.SerializeObject(app);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                var result = httpClient.PostAsync($"{options.Url}/instances", byteContent).Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var task = result.Content.ReadAsStringAsync();
+                    registrationResult = JsonConvert.DeserializeObject<RegistrationResult>(task.Result);
+                }
+            });
+
+            lifetime.ApplicationStopped.Register(() =>
+            {
+                if (RegistrationResult == null || string.IsNullOrEmpty(RegistrationResult.Id))
+                {
+                    return;
+                }
+
+                var httpClient = testClient ?? HttpClientHelper.GetHttpClient(false, ConnectionTimeoutMs);
+                var result = httpClient.DeleteAsync($"{options.Url}/instances/{RegistrationResult.Id}").Result;
+            });
+        }
+    }
+}
