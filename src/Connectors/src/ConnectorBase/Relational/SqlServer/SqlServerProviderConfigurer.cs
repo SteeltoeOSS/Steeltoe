cc39282671ec7c6ebe11502447d5edd7cbb5e1d9
Tim Hess <thess@pivotal.io>
2020-05-15 09:21:52 -0400

    fix merge issues

--- src/Connectors/src/ConnectorBase/Relational/SqlServer/SqlServerProviderConfigurer.cs
+++ src/Connectors/src/ConnectorBase/Relational/SqlServer/SqlServerProviderConfigurer.cs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Steeltoe.Common.Extensions;
 using Steeltoe.Connector.Services;
 using System;
 using System.Net;
