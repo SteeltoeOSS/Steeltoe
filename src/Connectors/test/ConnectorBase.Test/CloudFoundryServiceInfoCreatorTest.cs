15dbf9d0c84f34547b2574221fb3a5abc4e3a850
Tim Hess <thess@vmware.com>
2020-06-05 18:10:42 -0400

    cleanup

--- src/Connectors/test/ConnectorBase.Test/CloudFoundryServiceInfoCreatorTest.cs
+++ src/Connectors/test/ConnectorBase.Test/CloudFoundryServiceInfoCreatorTest.cs
@@ -79,7 +79,7 @@ namespace Steeltoe.Connector.Test
             var inst = CloudFoundryServiceInfoCreator.Instance(config);
             Assert.NotNull(inst);
             Assert.NotNull(inst.Factories);
-            Assert.Equal(13, inst.Factories.Count);
+            Assert.Equal(12, inst.Factories.Count);
         }
 
         [Fact]
