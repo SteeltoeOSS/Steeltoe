ad9943d4a7b5d0fff3e4faeb5f6d83d2d1a9f978
Travis Boatman <TBoatman@lighthouseglobal.com>
2020-08-11 18:57:20 -0700

    Add new MySqlConnector namespace

--- src/Connectors/src/ConnectorBase/MySql/MySqlTypeLocator.cs
+++ src/Connectors/src/ConnectorBase/MySql/MySqlTypeLocator.cs
@@ -20,7 +20,7 @@ namespace Steeltoe.Connector.MySql
         /// <summary>
         /// Gets a list of MySQL types that implement IDbConnection
         /// </summary>
-        public static string[] ConnectionTypeNames { get; internal set; } = new string[] { "MySql.Data.MySqlClient.MySqlConnection" };
+        public static string[] ConnectionTypeNames { get; internal set; } = new string[] { "MySql.Data.MySqlClient.MySqlConnection", "MySqlConnector.MySqlConnection" };
 
         /// <summary>
         /// Gets MySqlConnection from a MySQL Library
