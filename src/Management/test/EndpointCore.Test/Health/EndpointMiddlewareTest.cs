09cf876336036a39df734f936b4fa16c114287d0
hananiel <hananiel@gmail.com>
2020-11-18 20:42:01 -0500

    Fix issue with serialization(missing details); Fix msft health check status and description fields

--- src/Management/test/EndpointCore.Test/Health/EndpointMiddlewareTest.cs
+++ src/Management/test/EndpointCore.Test/Health/EndpointMiddlewareTest.cs
@@ -117,7 +117,8 @@ namespace Steeltoe.Management.Endpoint.Health.Test
             var health = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
             Assert.NotNull(health);
             Assert.True(health.ContainsKey("status"));
-            Assert.True(health.ContainsKey("diskSpace"));
+            Assert.True(health.ContainsKey("details"));
+            Assert.Contains("diskSpace", health["details"].ToString());
         }
 
         [Fact]
@@ -140,7 +141,8 @@ namespace Steeltoe.Management.Endpoint.Health.Test
             var health = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
             Assert.NotNull(health);
             Assert.True(health.ContainsKey("status"));
-            Assert.True(health.ContainsKey("diskSpace"));
+            Assert.True(health.ContainsKey("details"));
+            Assert.Contains("diskSpace", health["details"].ToString());
         }
 
         [Fact]
@@ -163,7 +165,8 @@ namespace Steeltoe.Management.Endpoint.Health.Test
             var health = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
             Assert.NotNull(health);
             Assert.True(health.ContainsKey("status"));
-            Assert.True(health.ContainsKey("diskSpace"));
+            Assert.True(health.ContainsKey("details"));
+            Assert.Contains("diskSpace", health["details"].ToString());
         }
 
         [Fact]
