77fdf547fc08eaec721d993ab7aa15784e39f8d2
Ken Swan <kswan@vmware.com>
2021-01-27 08:44:20 -0600

    add file header for authserviceinfo

--- src/Connectors/src/ConnectorBase/Services/OAuthServiceInfo.cs
+++ src/Connectors/src/ConnectorBase/Services/OAuthServiceInfo.cs
@@ -1,4 +1,8 @@
-﻿namespace Steeltoe.CloudFoundry.Connector.Services
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+namespace Steeltoe.CloudFoundry.Connector.Services
 {
     public class OAuthServiceInfo
     {
