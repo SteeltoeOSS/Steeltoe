e7512d1c34bdfd3d2ebc49c373836c74bb4847fb
Ken Swan <49496839+kenswan@users.noreply.github.com>
2021-01-28 09:20:57 -0600

    Add license info to test file
    Co-authored-by: Tim Hess <thess@vmware.com>

--- src/Security/test/Authentication.CloudFoundryCore.Test/CloudFoundryOAuthBuilderTest.cs
+++ src/Security/test/Authentication.CloudFoundryCore.Test/CloudFoundryOAuthBuilderTest.cs
@@ -1,4 +1,8 @@
-﻿using Steeltoe.Security.Authentication.CloudFoundry;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.﻿
+
+using Steeltoe.Security.Authentication.CloudFoundry;
 using Steeltoe.Security.Authentication.CloudFoundry.Test;
 using System;
 using System.Collections.Generic;
