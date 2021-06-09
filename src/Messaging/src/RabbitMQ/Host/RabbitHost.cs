4e2d2221ebf53ca18589e3e391fdc74ba400e21a
Ken Swan <kswan@vmware.com>
2021-06-09 11:56:20 -0500

    Add file headers

--- src/Messaging/src/RabbitMQ/Host/RabbitHost.cs
+++ src/Messaging/src/RabbitMQ/Host/RabbitHost.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Steeltoe.Common.Lifecycle;
 using System;
@@ -7,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace Steeltoe.Messaging.RabbitMQ.Host
 {
-    public class RabbitHost : IHost
+    public sealed class RabbitHost : IHost
     {
         public static IHostBuilder CreateDefaultBuilder() =>
             new RabbitHostBuilder(Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder());
--- src/Messaging/test/RabbitMQ.Test/Host/MockRabbitHostedService.cs
+++ src/Messaging/test/RabbitMQ.Test/Host/MockRabbitHostedService.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
