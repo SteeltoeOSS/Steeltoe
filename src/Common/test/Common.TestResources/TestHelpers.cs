6b960cbc63a5e952001260594926397958cccdee
Tim Hess <thess@vmware.com>
2022-02-07 12:58:11 -0500

    increase blame_hang_timeout, sonar for TestResources

--- src/Common/test/Common.TestResources/TestHelpers.cs
+++ src/Common/test/Common.TestResources/TestHelpers.cs
@@ -1,16 +1,6 @@
-﻿// Copyright 2017 the original author or authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
 
 #if NET6_0_OR_GREATER
 using Microsoft.AspNetCore.Builder;
@@ -20,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
 using System.Reflection;
 
@@ -92,7 +83,7 @@ namespace Steeltoe
                 ""application_id"": ""798c2495-fe75-49b1-88da-b81197f2bf06""
             }";
 
-        public static readonly Dictionary<string, string> _fastTestsConfiguration = new ()
+        public static readonly ImmutableDictionary<string, string> _fastTestsConfiguration = new Dictionary<string, string>()
         {
             { "spring:cloud:config:enabled", "false" },
             { "eureka:client:serviceUrl", "http://127.0.0.1" },
@@ -102,7 +93,7 @@ namespace Steeltoe
             { "redis:client:abortOnConnectFail", "false" },
             { "redis:client:connectTimeout", "1" },
             { "sqlserver:credentials:timeout", "1" }
-        };
+        }.ToImmutableDictionary();
 
 #if NET6_0_OR_GREATER
         public static WebApplicationBuilder GetTestWebApplicationBuilder(string[] args = null)
