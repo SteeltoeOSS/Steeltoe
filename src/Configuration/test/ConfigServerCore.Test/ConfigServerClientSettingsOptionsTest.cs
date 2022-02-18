1d5d5c1cc5c494e76610917f994eddc9bc6279cf
Dave Tillman <dtillman@vmware.com>
2022-02-18 14:00:45 -0700

    Remove duplicate asserts

--- src/Configuration/test/ConfigServerCore.Test/ConfigServerClientSettingsOptionsTest.cs
+++ src/Configuration/test/ConfigServerCore.Test/ConfigServerClientSettingsOptionsTest.cs
@@ -32,31 +32,6 @@ namespace Steeltoe.Extensions.Configuration.ConfigServerCore.Test
             var options = service.Value;
             Assert.NotNull(options);
             TestHelper.VerifyDefaults(options.Settings);
-
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_PROVIDER_ENABLED, options.Enabled);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_FAILFAST, options.FailFast);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_URI, options.Uri);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_ENVIRONMENT, options.Environment);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_ACCESS_TOKEN_URI, options.AccessTokenUri);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_CLIENT_ID, options.ClientId);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_CLIENT_SECRET, options.ClientSecret);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_CERTIFICATE_VALIDATION, options.ValidateCertificates);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_INITIAL_RETRY_INTERVAL, options.RetryInitialInterval);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_MAX_RETRY_ATTEMPTS, options.RetryAttempts);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_RETRY_ENABLED, options.RetryEnabled);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_RETRY_MULTIPLIER, options.RetryMultiplier);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_MAX_RETRY_INTERVAL, options.RetryMaxInterval);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_TIMEOUT_MILLISECONDS, options.Timeout);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_VAULT_TOKEN_RENEW_RATE, options.TokenRenewRate);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_VAULT_TOKEN_TTL, options.TokenTtl);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_DISCOVERY_ENABLED, options.DiscoveryEnabled);
-            Assert.Equal(ConfigServerClientSettings.DEFAULT_CONFIGSERVER_SERVICEID, options.DiscoveryServiceId);
-            Assert.Null(options.Name);
-            Assert.Null(options.Label);
-            Assert.Null(options.Username);
-            Assert.Null(options.Password);
-            Assert.Null(options.Token);
-            Assert.Null(options.Headers);
         }
 
         [Fact]
