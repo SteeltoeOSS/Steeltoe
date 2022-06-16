858978591bffc03c31fa6995a7601ec83b36527d
bart-vmware <104792814+bart-vmware@users.noreply.github.com>
2022-06-16 17:11:29 +0200

    Add suppression in code for S4275: Getters and setters should access the expected fields

--- src/Integration/src/IntegrationBase/MessagingTemplate.cs
+++ src/Integration/src/IntegrationBase/MessagingTemplate.cs
@@ -39,8 +39,10 @@ public class MessagingTemplate : MessageChannelTemplate
 
     public override IMessageChannel DefaultReceiveDestination
     {
+#pragma warning disable S4275 // Getters and setters should access the expected fields
         get => base.DefaultReceiveDestination;
         set => base.DefaultReceiveDestination = base.DefaultSendDestination = value;
+#pragma warning restore S4275 // Getters and setters should access the expected fields
     }
 
     public override IMessageChannel DefaultSendDestination
