81fa8d3893eb8b14cca5a24df33e240c31df770a
bart-vmware <104792814+bart-vmware@users.noreply.github.com>
2022-07-26 15:27:10 +0200

    split into steps

--- build/cleanup-code-in-pull-request.ps1
+++ build/cleanup-code-in-pull-request.ps1
@@ -10,9 +10,9 @@ if (-not $targetBranch) {
 }
 
 # We're going to push changes, so we need to be on the PR branch instead of the detached head containing the merge result.
-Write-Host "Switching to branch $env:SYSTEM_PULLREQUEST_SOURCEBRANCH"
-git checkout $env:SYSTEM_PULLREQUEST_SOURCEBRANCH
-EnsureSucceeded
+#Write-Host "Switching to branch $env:SYSTEM_PULLREQUEST_SOURCEBRANCH"
+#git checkout $env:SYSTEM_PULLREQUEST_SOURCEBRANCH
+#EnsureSucceeded
 
 # Find the most-recent common ancestor to diff against. Using the target branch name is incorrect because this PR may be outdated.
 $baseCommitHash = git merge-base origin/$env:SYSTEM_PULLREQUEST_SOURCEBRANCH origin/$env:SYSTEM_PULLREQUEST_TARGETBRANCH
@@ -22,11 +22,11 @@ $headCommitHash = git rev-parse "HEAD"
 EnsureSucceeded
 Write-Host "Using commit range for cleanup: $baseCommitHash..$headCommitHash"
 
-dotnet tool restore
-EnsureSucceeded
+#dotnet tool restore
+#EnsureSucceeded
 
-dotnet restore src
-EnsureSucceeded
+#dotnet restore src
+#EnsureSucceeded
 
 dotnet regitlint -s Steeltoe.All.sln --print-command --skip-tool-check --disable-jb-path-hack --jb-profile="Steeltoe Full Cleanup" --jb --properties:Configuration=Release --jb --verbosity=WARN -f commits -a $headCommitHash -b $baseCommitHash
 EnsureSucceeded
