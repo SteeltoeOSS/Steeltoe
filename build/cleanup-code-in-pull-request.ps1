9267c78c53bd9a7415ad4b0fb643a474ac57b9b6
bart-vmware <104792814+bart-vmware@users.noreply.github.com>
2022-07-26 13:10:39 +0200

    Update cleanup-code-in-pull-request.ps1

--- build/cleanup-code-in-pull-request.ps1
+++ build/cleanup-code-in-pull-request.ps1
@@ -4,31 +4,48 @@ function EnsureSucceeded {
   }
 }
 
-$jsonBody = @"
-{
-  "comments": [
-    {
-      "parentCommentId": 0,
-      "content": "Example comment from pipeline.",
-      "commentType": 2
-    }
-  ],
-  "status": 0
-}
+
+#Going to create the comment in an Active state, assuming it needs to be resolved
+#See https://docs.microsoft.com/en-us/dotnet/api/microsoft.teamfoundation.sourcecontrol.webapi.commentthreadstatus?view=azure-devops-dotnet
+$StatusCode = 1 
+
+$Stuff = $env:Build_Repository_Name
+$Things = "Other things you might want in the message"
+
+#Build Up a Markdown Message to 
+$Markdown = @"
+## Markdown Message here
+|Column0 |Column1|
+|--------|---------|
+|$Stuff|$Things|  
 "@
 
-try {
-  Write-Host "SYSTEM_ACCESSTOKEN = $env:SYSTEM_ACCESSTOKEN"
+#Build the JSON body up
+$body = @"
+{
+    "comments": [
+      {
+        "parentCommentId": 0,
+        "content": "$Markdown",
+        "commentType": 1
+      }
+    ],
+    "status": $StatusCode 
+  }
+"@
 
-  $url = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$env:SYSTEM_TEAMPROJECTID/_apis/git/repositories/$($env:BUILD_REPOSITORY_NAME)/pullRequests/$($env:SYSTEM_PULLREQUEST_PULLREQUESTID)/threads?api-version=6.0"
-  Write-Host "Posting PR comment to $url with body $jsonBody"
-  $response = Invoke-RestMethod -Uri $url -Method POST -Headers @{Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"} -Body $jsonBody -ContentType application/json-patch+json
-  if ($response -eq $Null) {
-    Write-Host "Failed to post PR comment."
+Write-Debug $Body
+#Post the message to the Pull Request
+#https://docs.microsoft.com/en-us/rest/api/azure/devops/git/pull%20request%20threads?view=azure-devops-rest-5.1
+try {
+    $url = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$env:SYSTEM_TEAMPROJECTID/_apis/git/repositories/$($env:Build_Repository_Name)/pullRequests/$($env:System_PullRequest_PullRequestId)/threads?api-version=5.1"
+    Write-Host "URL: $url"
+    $response = Invoke-RestMethod -Uri $url -Method POST -Headers @{Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"} -Body $Body -ContentType application/json
+  if ($response -ne $Null) {
+    Write-Host "*******************Bingo*********************************"
   }
 }
 catch {
-  Write-Host "Comment post failed."
   Write-Error $_
   Write-Error $_.Exception.Message
 }
