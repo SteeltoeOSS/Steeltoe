4e0a6c1778d3ba308fa543e334463e8d9af589c8
bart-vmware <104792814+bart-vmware@users.noreply.github.com>
2022-07-26 14:22:41 +0200

    more

--- build/cleanup-code-in-pull-request.ps1
+++ build/cleanup-code-in-pull-request.ps1
@@ -4,34 +4,4 @@ function EnsureSucceeded {
   }
 }
 
-$jsonBody = @"
-{
-  "comments": [
-    {
-      "parentCommentId": 0,
-      "content": "Example comment from pipeline.",
-      "commentType": 1
-    }
-  ],
-  "status": 1
-}
-"@
-
-try {
-  Write-Host "SYSTEM_TEAMFOUNDATIONCOLLECTIONURI = $env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI"
-  Write-Host "SYSTEM_TEAMPROJECTID = $env:SYSTEM_TEAMPROJECTID"
-  Write-Host "BUILD_REPOSITORY_NAME = $env:BUILD_REPOSITORY_NAME"
-  Write-Host "SYSTEM_PULLREQUEST_PULLREQUESTID = $env:SYSTEM_PULLREQUEST_PULLREQUESTID"
-
-  $url = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$env:SYSTEM_TEAMPROJECTID/_apis/git/repositories/Steeltoe/pullRequests/$($env:SYSTEM_PULLREQUEST_PULLREQUESTID)/threads?api-version=5.1"
-  Write-Host "Posting PR comment to $url with body $jsonBody"
-  $response = Invoke-RestMethod -Uri $url -Method POST -Headers @{Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"} -Body $jsonBody -ContentType application/json
-  if ($response -eq $Null) {
-    Write-Host "Failed to post PR comment."
-  }
-}
-catch {
-  Write-Host "Comment post failed."
-  Write-Error $_
-  Write-Error $_.Exception.Message
-}
+Write-Host "Hello world"
