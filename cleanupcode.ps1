5afda30e2831ec209bba9a7dd02ce8765cf21158
bart-vmware <104792814+bart-vmware@users.noreply.github.com>
2022-08-01 10:58:26 +0200

    Update regitlint

--- cleanupcode.ps1
+++ cleanupcode.ps1
@@ -1,10 +1,10 @@
 #Requires -Version 7.0
 
-# This script reformats the entire codebase to make it compliant with our coding guidelines.
+# This script reformats (part of) the codebase to make it compliant with our coding guidelines.
 
 param(
-    # Branch name or base commit hash to reformat only the subset of changed files. Leave empty for all files.
-    [string] $diff
+    # Git branch name or base commit hash to reformat only the subset of changed files. Omit for all files.
+    [string] $revision
 )
 
 function VerifySuccessExitCode {
@@ -19,21 +19,26 @@ VerifySuccessExitCode
 dotnet restore
 VerifySuccessExitCode
 
-if ($diff) {
+if ($revision) {
     $headCommitHash = git rev-parse HEAD
     VerifySuccessExitCode
 
-    $baseCommitHash = git rev-parse $diff
+    $baseCommitHash = git rev-parse $revision
     VerifySuccessExitCode
 
-    echo "Using commit range for cleanup: $baseCommitHash..$headCommitHash, including staged/unstaged files."
-
-    dotnet regitlint -s Steeltoe.All.sln --print-command --skip-tool-check --disable-jb-path-hack --jb-profile="Steeltoe Full Cleanup" --jb --properties:Configuration=Release --jb --verbosity=WARN -f staged,modified,commits -a $headCommitHash -b $baseCommitHash
-    VerifySuccessExitCode
+    if ($baseCommitHash -eq $headCommitHash) {
+        Write-Output "Running code cleanup on staged/unstaged files."
+        dotnet regitlint -s src/Steeltoe.All.sln --print-command --skip-tool-check --jb-profile="Steeltoe Full Cleanup" --jb --properties:Configuration=Release --jb --verbosity=WARN -f staged,modified
+        VerifySuccessExitCode
+    }
+    else {
+        Write-Output "Running code cleanup on commit range $baseCommitHash..$headCommitHash, including staged/unstaged files."
+        dotnet regitlint -s src/Steeltoe.All.sln --print-command --skip-tool-check --jb-profile="Steeltoe Full Cleanup" --jb --properties:Configuration=Release --jb --verbosity=WARN -f staged,modified,commits -a $headCommitHash -b $baseCommitHash
+        VerifySuccessExitCode
+    }
 }
 else {
-    echo "Running cleanup on all files."
-
-    dotnet regitlint -s Steeltoe.All.sln --print-command --skip-tool-check --disable-jb-path-hack --jb-profile="Steeltoe Full Cleanup" --jb --properties:Configuration=Release --jb --verbosity=WARN
+    Write-Output "Running code cleanup on all files."
+    dotnet regitlint -s src/Steeltoe.All.sln --print-command --skip-tool-check --jb-profile="Steeltoe Full Cleanup" --jb --properties:Configuration=Release --jb --verbosity=WARN
     VerifySuccessExitCode
 }
