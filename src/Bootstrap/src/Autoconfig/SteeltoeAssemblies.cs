c5a7b333901785af8765e2faf8b72f28781248da
bart-vmware <104792814+bart-vmware@users.noreply.github.com>
2022-08-15 21:30:55 +0200

    Fix formatting

--- src/Bootstrap/src/Autoconfig/SteeltoeAssemblies.cs
+++ src/Bootstrap/src/Autoconfig/SteeltoeAssemblies.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 // ReSharper disable MemberCanBeInternal
+
 namespace Steeltoe.Bootstrap.Autoconfig;
 
 public static class SteeltoeAssemblies
