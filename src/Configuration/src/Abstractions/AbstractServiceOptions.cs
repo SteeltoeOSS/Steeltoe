eeaf60c330eb120cc82940e9850ccd6cdf92666a
bart-vmware <104792814+bart-vmware@users.noreply.github.com>
2022-08-25 12:39:41 +0200

    Removed IServicesInfo (never used)

--- src/Configuration/src/Abstractions/AbstractServiceOptions.cs
+++ src/Configuration/src/Abstractions/AbstractServiceOptions.cs
@@ -8,7 +8,7 @@ using Steeltoe.Common.Options;
 
 namespace Steeltoe.Extensions.Configuration;
 
-public abstract class AbstractServiceOptions : AbstractOptions, IServicesInfo
+public abstract class AbstractServiceOptions : AbstractOptions
 {
     public virtual string ConfigurationPrefix { get; protected set; } = "services";
 
@@ -49,6 +49,12 @@ public abstract class AbstractServiceOptions : AbstractOptions, IServicesInfo
     {
     }
 
+    /// <summary>
+    /// Retrieves a list of all service instances for all service types.
+    /// </summary>
+    /// <returns>
+    /// A complete list of service instances known to the application.
+    /// </returns>
     public IEnumerable<Service> GetServicesList()
     {
         var results = new List<Service>();
@@ -64,6 +70,18 @@ public abstract class AbstractServiceOptions : AbstractOptions, IServicesInfo
         return results;
     }
 
+    /// <summary>
+    /// Retrieves a list of all service instances of a given service type.
+    /// </summary>
+    /// <param name="serviceType">
+    /// String value that identifies the service type. May be platform/broker/version dependent.
+    /// </param>
+    /// <remarks>
+    /// Sample values include: p-mysql, azure-mysql-5-7, p-configserver, p.configserver.
+    /// </remarks>
+    /// <returns>
+    /// A list of service instances configured under the given type.
+    /// </returns>
     public IEnumerable<Service> GetInstancesOfType(string serviceType)
     {
         Services.TryGetValue(serviceType, out IEnumerable<Service> services);
