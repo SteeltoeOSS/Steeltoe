a99f0600b999a485e1ae89aad55d2b2c9254975e
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2023-05-19 12:28:11 +0200

    MongoDB: Replace TypeLocator usage with shims

--- src/Connectors/src/Connectors/MongoDb/MongoDbWebApplicationBuilderExtensions.cs
+++ src/Connectors/src/Connectors/MongoDb/MongoDbWebApplicationBuilderExtensions.cs
@@ -2,40 +2,52 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Steeltoe.Common;
 using Steeltoe.Common.HealthChecks;
+using Steeltoe.Connectors.MongoDb.RuntimeTypeAccess;
 
 namespace Steeltoe.Connectors.MongoDb;
 
 public static class MongoDbWebApplicationBuilderExtensions
 {
-    private static readonly Type ConnectionType = MongoDbTypeLocator.MongoClient;
-    private static readonly Type ConnectionInterface = MongoDbTypeLocator.MongoClientInterface;
-
     public static WebApplicationBuilder AddMongoDb(this WebApplicationBuilder builder)
     {
+        return AddMongoDb(builder, new MongoDbPackageResolver());
+    }
+
+    private static WebApplicationBuilder AddMongoDb(this WebApplicationBuilder builder, MongoDbPackageResolver packageResolver)
+    {
         ArgumentGuard.NotNull(builder);
+        ArgumentGuard.NotNull(packageResolver);
 
         var connectionStringPostProcessor = new MongoDbConnectionStringPostProcessor();
 
-        Func<MongoDbOptions, string, object> createMongoClient = (options, _) => Activator.CreateInstance(ConnectionType, options.ConnectionString);
+        Func<MongoDbOptions, string, object> createMongoClient = (options, _) =>
+            MongoClientShim.CreateInstance(packageResolver, options.ConnectionString).Instance;
 
         BaseWebApplicationBuilderExtensions.RegisterConfigurationSource(builder.Configuration, connectionStringPostProcessor);
-        BaseWebApplicationBuilderExtensions.RegisterNamedOptions<MongoDbOptions>(builder, "mongodb", CreateHealthContributor);
-        BaseWebApplicationBuilderExtensions.RegisterConnectorFactory(builder.Services, ConnectionInterface, false, createMongoClient);
+
+        BaseWebApplicationBuilderExtensions.RegisterNamedOptions<MongoDbOptions>(builder, "mongodb",
+            (serviceProvider, bindingName) => CreateHealthContributor(serviceProvider, bindingName, packageResolver));
+
+        BaseWebApplicationBuilderExtensions.RegisterConnectorFactory(builder.Services, packageResolver.MongoClientInterface.Type, false, createMongoClient);
 
         return builder;
     }
 
-    private static IHealthContributor CreateHealthContributor(IServiceProvider serviceProvider, string bindingName)
+    private static IHealthContributor CreateHealthContributor(IServiceProvider serviceProvider, string bindingName, MongoDbPackageResolver packageResolver)
     {
-        string connectionString = ConnectorFactoryInvoker.GetConnectionString<MongoDbOptions>(serviceProvider, bindingName, ConnectionInterface);
+        string connectionString =
+            ConnectorFactoryInvoker.GetConnectionString<MongoDbOptions>(serviceProvider, bindingName, packageResolver.MongoClientInterface.Type);
+
         string serviceName = $"MongoDB-{bindingName}";
         string hostName = GetHostNameFromConnectionString(connectionString);
-        object mongoClient = ConnectorFactoryInvoker.GetConnection<MongoDbOptions>(serviceProvider, bindingName, ConnectionInterface);
+        object mongoClient = ConnectorFactoryInvoker.GetConnection<MongoDbOptions>(serviceProvider, bindingName, packageResolver.MongoClientInterface.Type);
         var logger = serviceProvider.GetRequiredService<ILogger<MongoDbHealthContributor>>();
 
         return new MongoDbHealthContributor(mongoClient, serviceName, hostName, logger);
@@ -48,6 +60,6 @@ public static class MongoDbWebApplicationBuilderExtensions
             ConnectionString = connectionString
         };
 
-        return (string)builder["server"];
+        return (string)builder["server"]!;
     }
 }
