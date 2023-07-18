a9b068de21f099a8fb5ce7cd85591ce94ddc58fa
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2023-07-18 16:25:52 +0200

    add new file

---
+++ src/Common/test/Common.Http.Test/TestInnerDelegatingHandler2.cs
@@ -0,0 +1,17 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using System.Net;
+
+namespace Steeltoe.Common.Http.Test;
+
+public class TestInnerDelegatingHandler2 : DelegatingHandler
+{
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+        responseMessage.Headers.Add("requestUri", request.RequestUri.AbsoluteUri);
+        return Task.Factory.StartNew(() => responseMessage, cancellationToken);
+    }
+}
