ce90ff9a090e2f2169774a3320ad4a6c781dac2b
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2023-08-14 15:39:40 +0200

    Fixed: sign same package multiple times; removed duplicate logging; linux compatibility

--- build/sign-packages.ps1
+++ build/sign-packages.ps1
@@ -2,8 +2,8 @@
 $currentDirectory = split-path $MyInvocation.MyCommand.Definition
 
 # See if we have the ClientSecret available
-if([string]::IsNullOrEmpty($Env:SignClientSecret)){
-	Write-Host "Client Secret not found, not signing packages"
+if (-not $env:SignClientSecret) {
+	Write-Host "Client Secret not found, not signing packages."
 	return;
 }
 
@@ -11,16 +11,12 @@ dotnet tool install --tool-path . SignClient
 
 # Setup Variables we need to pass into the sign client tool
 
-$appSettings = "$currentDirectory\appsettings.json"
+$appSettings = Join-Path $currentDirectory 'appsettings.json'
 
-$nupkgs = Get-ChildItem $ArtifactDirectory\Steeltoe*.*nupkg -recurse | Select-Object -ExpandProperty FullName
+$nupkgs = Get-ChildItem $env:ArtifactDirectory/Steeltoe*.*nupkg -recurse | Select-Object -ExpandProperty FullName
 
-foreach ($nupkg in $nupkgs){
-	Write-Host "Submitting $nupkg for signing"
-
-	.\SignClient 'sign' -c $appSettings -i $nupkg -r $Env:SignClientUser -s $Env:SignClientSecret -n 'Steeltoe' -d 'Steeltoe' -u 'https://github.com/SteeltoeOSS' 
-
-	Write-Host "Finished signing $nupkg"
+foreach ($nupkg in $nupkgs) {
+	./SignClient 'sign' -c $appSettings -i $nupkg -r $env:SignClientUser -s $env:SignClientSecret -n 'Steeltoe' -d 'Steeltoe' -u 'https://github.com/SteeltoeOSS' 
 }
 
-Write-Host "Sign-package complete"
+Write-Host "Sign-packages completed."
