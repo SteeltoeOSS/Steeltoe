57a57a67bc7b8abc95aa9b8a710b8f60a8f70f57
Mateusz Cieciura <mateusz.cieciura@gmail.com>
2023-08-31 11:19:57 +0200

    Fix tests

--- src/Discovery/test/Consul.Test/Discovery/ConsulServiceInstanceTest.cs
+++ src/Discovery/test/Consul.Test/Discovery/ConsulServiceInstanceTest.cs
@@ -20,10 +20,11 @@ public sealed class ConsulServiceInstanceTest
                 Service = "ServiceId",
                 Address = "foo.bar.com",
                 Port = 1234,
-                Tags = new[]
+                Tags = new[] { "tag1", "tag2" },
+                Meta = new Dictionary<string, string>
                 {
-                    "foo=bar",
-                    "secure=true"
+                    ["foo"] = "bar",
+                    ["secure"] = "true"
                 }
             }
         };
@@ -33,11 +34,14 @@ public sealed class ConsulServiceInstanceTest
         Assert.Equal("ServiceId", si.ServiceId);
         Assert.True(si.IsSecure);
         Assert.Equal(1234, si.Port);
+        Assert.Equal(2, si.Tags.Length);
+        Assert.Contains("tag1", si.Tags);
+        Assert.Contains("tag2", si.Tags);
         Assert.Equal(2, si.Metadata.Count);
-        Assert.Contains("foo", si.Metadata.Keys);
-        Assert.Contains("secure", si.Metadata.Keys);
-        Assert.Contains("bar", si.Metadata.Values);
-        Assert.Contains("true", si.Metadata.Values);
+        Assert.Contains(si.Metadata, x => x.Key == "foo");
+        Assert.Equal("bar", si.Metadata["foo"]);
+        Assert.Contains(si.Metadata, x => x.Key == "secure");
+        Assert.Equal("true", si.Metadata["secure"]);
         Assert.Equal(new Uri("https://foo.bar.com:1234"), si.Uri);
     }
 }
