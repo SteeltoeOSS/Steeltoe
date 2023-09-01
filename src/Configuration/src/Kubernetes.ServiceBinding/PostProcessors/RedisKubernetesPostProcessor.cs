c38c05817d66998ed1124af0d043ab9e6d28a471
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2023-09-01 14:32:36 +0200

    Fix broken URLs

--- src/Configuration/src/Kubernetes.ServiceBinding/PostProcessors/RedisKubernetesPostProcessor.cs
+++ src/Configuration/src/Kubernetes.ServiceBinding/PostProcessors/RedisKubernetesPostProcessor.cs
@@ -21,7 +21,7 @@ internal sealed class RedisKubernetesPostProcessor : IConfigurationPostProcessor
             // Mapping from Kubernetes secrets to driver-specific connection string parameters.
             // At the time of writing (June 2023), there's no complete official documentation for the available secrets. Some pointers:
             // - Generic secrets: https://github.com/servicebinding/spec#well-known-secret-entries
-            // - Input keys used at https://github.com/spring-cloud/spring-cloud-bindings/blob/main/src/main/java/org/springframework/cloud/bindings/boot/RedisBindingsPropertiesProcessor.java
+            // - Input keys used at https://github.com/spring-cloud/spring-cloud-bindings/blob/main/spring-cloud-bindings/src/main/java/org/springframework/cloud/bindings/boot/RedisBindingsPropertiesProcessor.java
 
             mapper.MapFromTo("host", "host");
             mapper.MapFromTo("port", "port");
