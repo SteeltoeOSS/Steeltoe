a8a20968aadd1c28c2eaa898b6905635dc215917
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2023-10-03 13:31:29 +0200

    Fix S3878: Remove this array creation and simply pass the elements

--- src/Messaging/test/RabbitMQ.Test/Core/RabbitAdminDeclarationTest.cs
+++ src/Messaging/test/RabbitMQ.Test/Core/RabbitAdminDeclarationTest.cs
@@ -343,10 +343,7 @@ public sealed class RabbitAdminDeclarationTest : IClassFixture<RabbitAdminDeclar
         queue.SetAdminsThatShouldDeclare(admin1);
         Assert.Single(queue.DeclaringAdmins);
 
-        queue.SetAdminsThatShouldDeclare(new object[]
-        {
-            null
-        });
+        queue.SetAdminsThatShouldDeclare(null);
 
         Assert.Empty(queue.DeclaringAdmins);
         queue.SetAdminsThatShouldDeclare(admin1, admin2);
