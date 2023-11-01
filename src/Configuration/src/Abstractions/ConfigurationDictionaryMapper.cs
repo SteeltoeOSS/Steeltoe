9e4213f834575cd460b9673e4fb14e0c2fbdb5e8
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2023-11-01 12:43:10 +0100

    Add Connector support for VMware Postgres TAS tile

--- src/Configuration/src/Abstractions/ConfigurationDictionaryMapper.cs
+++ src/Configuration/src/Abstractions/ConfigurationDictionaryMapper.cs
@@ -4,6 +4,7 @@
 
 #nullable enable
 
+using System.Text;
 using Microsoft.Extensions.Configuration;
 
 namespace Steeltoe.Configuration;
@@ -27,6 +28,43 @@ internal abstract class ConfigurationDictionaryMapper
         SetToValue(toKey, value);
 
         return value;
+    }
+
+    public string? MapArrayFromTo(string fromArrayKey, string toKey, string separator)
+    {
+        int arrayIndex = 0;
+        var toValueBuilder = new StringBuilder();
+
+        while (true)
+        {
+            string arrayElementKey = $"{BindingKey}{fromArrayKey}:{arrayIndex}";
+
+            if (!ConfigurationData.TryGetValue(arrayElementKey, out string? arrayElementValue))
+            {
+                break;
+            }
+
+            if (!string.IsNullOrEmpty(arrayElementValue))
+            {
+                if (toValueBuilder.Length > 0)
+                {
+                    toValueBuilder.Append(separator);
+                }
+
+                toValueBuilder.Append(arrayElementValue);
+            }
+
+            arrayIndex++;
+        }
+
+        if (toValueBuilder.Length > 0)
+        {
+            string toValue = toValueBuilder.ToString();
+            SetToValue(toKey, toValue);
+            return toValue;
+        }
+
+        return null;
     }
 
     public string? MapFromAppendTo(string fromKey, string appendToKey, string separator)
