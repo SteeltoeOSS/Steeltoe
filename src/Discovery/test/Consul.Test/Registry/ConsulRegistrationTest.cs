8815e6a7d4c93409c79a7e3864069ec4f04979ed
Mateusz Cieciura <mateusz.cieciura@gmail.com>
2024-01-20 18:49:22 +0100

    Consul :: Port tests to cover health check path being used when heartbeat is off

--- src/Discovery/test/Consul.Test/Registry/ConsulRegistrationTest.cs
+++ src/Discovery/test/Consul.Test/Registry/ConsulRegistrationTest.cs
@@ -267,4 +267,56 @@ public sealed class ConsulRegistrationTest
         Assert.NotNull(reg.Service.Check);
         Assert.NotNull(reg.Service.Tags);
     }
+
+    [Fact]
+    public void CreateCheck_WhenHealthCheckPathIsSetAndHeartbeatIsDisabled_ThenShouldSetHttp()
+    {
+        const string path = "/my/custom/health";
+        var options = new ConsulDiscoveryOptions
+        {
+            HealthCheckPath = path,
+            Heartbeat = new ConsulHeartbeatOptions
+            {
+                Enabled = false
+            }
+        };
+
+        var check = ConsulRegistration.CreateCheck(1234, options);
+
+        Assert.Contains(path, check.HTTP);
+    }
+
+    [Fact]
+    public void CreateCheck_WhenHealthCheckPathIsSetAndHeartbeatIsEnabled_ThenHttpShouldBeNull()
+    {
+        const string path = "/my/custom/health";
+        var options = new ConsulDiscoveryOptions
+        {
+            HealthCheckPath = path,
+            Heartbeat = new ConsulHeartbeatOptions
+            {
+                Enabled = true
+            }
+        };
+
+        var check = ConsulRegistration.CreateCheck(1234, options);
+
+        Assert.Null(check.HTTP);
+    }
+
+    [Fact]
+    public void CreateCheck_WhenHeartbeatIsDisabledAndPortIsANegativeNumber_ThenShouldThrow()
+    {
+        var options = new ConsulDiscoveryOptions
+        {
+            Heartbeat = new ConsulHeartbeatOptions()
+            {
+                Enabled = false
+            }
+        };
+
+        const int port = -1234;
+
+        Assert.Throws<ArgumentException>(() => ConsulRegistration.CreateCheck(port, options));
+    }
 }
