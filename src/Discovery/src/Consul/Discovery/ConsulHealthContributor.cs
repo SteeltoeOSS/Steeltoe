a97ca6f59b0dcb268af761a106671900b96945fb
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2024-01-24 12:59:35 +0100

    Cleanup ConsulHealthContributor

--- src/Discovery/src/Consul/Discovery/ConsulHealthContributor.cs
+++ src/Discovery/src/Consul/Discovery/ConsulHealthContributor.cs
@@ -2,9 +2,9 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
+
 using Consul;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Steeltoe.Common;
 using Steeltoe.Common.HealthChecks;
 using HealthStatus = Steeltoe.Common.HealthChecks.HealthStatus;
@@ -12,26 +12,11 @@ using HealthStatus = Steeltoe.Common.HealthChecks.HealthStatus;
 namespace Steeltoe.Discovery.Consul.Discovery;
 
 /// <summary>
-/// A Health contributor which provides the health of the Consul server connection.
+/// A health contributor that provides the health of the Consul server connection.
 /// </summary>
-public class ConsulHealthContributor : IHealthContributor
+public sealed class ConsulHealthContributor : IHealthContributor
 {
     private readonly IConsulClient _client;
-    private readonly IOptionsMonitor<ConsulDiscoveryOptions> _optionsMonitor;
-    private readonly ConsulDiscoveryOptions _options;
-
-    internal ConsulDiscoveryOptions Options
-    {
-        get
-        {
-            if (_optionsMonitor != null)
-            {
-                return _optionsMonitor.CurrentValue;
-            }
-
-            return _options;
-        }
-    }
 
     public string Id => "consul";
 
@@ -39,62 +24,30 @@ public class ConsulHealthContributor : IHealthContributor
     /// Initializes a new instance of the <see cref="ConsulHealthContributor" /> class.
     /// </summary>
     /// <param name="client">
-    /// a Consul client to use for health checks.
+    /// The Consul client to use for health checks.
     /// </param>
-    /// <param name="options">
-    /// configuration options.
-    /// </param>
-    /// <param name="logger">
-    /// optional logger.
-    /// </param>
-    public ConsulHealthContributor(IConsulClient client, ConsulDiscoveryOptions options, ILogger<ConsulHealthContributor> logger = null)
+    public ConsulHealthContributor(IConsulClient client)
     {
         ArgumentGuard.NotNull(client);
-        ArgumentGuard.NotNull(options);
 
         _client = client;
-        _options = options;
     }
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="ConsulHealthContributor" /> class.
-    /// </summary>
-    /// <param name="client">
-    /// a Consul client to use for health checks.
-    /// </param>
-    /// <param name="optionsMonitor">
-    /// Provides access to <see cref="ConsulDiscoveryOptions" />.
-    /// </param>
-    /// <param name="logger">
-    /// optional logger.
-    /// </param>
-    public ConsulHealthContributor(IConsulClient client, IOptionsMonitor<ConsulDiscoveryOptions> optionsMonitor, ILogger<ConsulHealthContributor> logger = null)
+    /// <inheritdoc />
+    public async Task<HealthCheckResult?> CheckHealthAsync(CancellationToken cancellationToken)
     {
-        ArgumentGuard.NotNull(client);
-        ArgumentGuard.NotNull(optionsMonitor);
-
-        _client = client;
-        _optionsMonitor = optionsMonitor;
-    }
-
-    /// <summary>
-    /// Computes the health of the Consul server connection.
-    /// </summary>
-    /// <param name="cancellationToken">
-    /// The token to monitor for cancellation requests.
-    /// </param>
-    /// <returns>
-    /// The health check result.
-    /// </returns>
-    public async Task<HealthCheckResult> CheckHealthAsync(CancellationToken cancellationToken)
-    {
-        var result = new HealthCheckResult();
         string leaderStatus = await GetLeaderStatusAsync(cancellationToken);
         Dictionary<string, string[]> services = await GetCatalogServicesAsync(cancellationToken);
-        result.Status = HealthStatus.Up;
-        result.Details.Add("leader", leaderStatus);
-        result.Details.Add("services", services);
-        return result;
+
+        return new HealthCheckResult
+        {
+            Status = HealthStatus.Up,
+            Details =
+            {
+                ["leader"] = leaderStatus,
+                ["services"] = services
+            }
+        };
     }
 
     internal Task<string> GetLeaderStatusAsync(CancellationToken cancellationToken)
