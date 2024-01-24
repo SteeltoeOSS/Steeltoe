caad51b8f1d49c9c3374f0bfd4ea653b806d9433
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2024-01-24 13:14:33 +0100

    Cleanup DateTimeConversions

--- src/Discovery/test/Consul.Test/Util/DateTimeConversionsTest.cs
+++ src/Discovery/test/Consul.Test/Util/DateTimeConversionsTest.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
+
 using Steeltoe.Discovery.Consul.Util;
 using Xunit;
 
@@ -12,7 +14,7 @@ public sealed class DateTimeConversionsTest
     [Fact]
     public void ToTimeSpan_ReturnsExpected()
     {
-        Assert.Throws<ArgumentNullException>(() => DateTimeConversions.ToTimeSpan(null));
+        Assert.Throws<ArgumentNullException>(() => DateTimeConversions.ToTimeSpan(null!));
         Assert.Throws<ArgumentException>(() => DateTimeConversions.ToTimeSpan(string.Empty));
         Assert.Throws<ArgumentException>(() => DateTimeConversions.ToTimeSpan(" "));
         Assert.Throws<FormatException>(() => DateTimeConversions.ToTimeSpan("foobar"));
