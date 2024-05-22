eba83931adaa218101790d043e6ee2f0dc0047ef
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2024-05-22 17:41:35 +0200

    Remove workarounds for old Sonar bugs

--- src/Discovery/src/Eureka/Configuration/EurekaInstanceOptions.cs
+++ src/Discovery/src/Eureka/Configuration/EurekaInstanceOptions.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Logging;
 using Steeltoe.Common;
 using Steeltoe.Discovery.Eureka.AppInfo;
 
-// Workaround for Sonar bug, which has been fixed in SonarAnalyzer.CSharp v9.12.
+// Workaround for Sonar bug, which incorrectly flags boolean expression as redundant.
 #pragma warning disable S2589 // Boolean expressions should not be gratuitous
 
 namespace Steeltoe.Discovery.Eureka.Configuration;
