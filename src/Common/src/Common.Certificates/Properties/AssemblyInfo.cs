5cae4a50b3c8214ff84a52e884a83d6b69808210
Tim Hess <tim.hess@broadcom.com>
2024-06-17 16:20:43 -0500

    cleanup

--- src/Common/src/Common.Certificates/Properties/AssemblyInfo.cs
+++ src/Common/src/Common.Certificates/Properties/AssemblyInfo.cs
@@ -4,8 +4,6 @@
 
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Steeltoe.Bootstrap.AutoConfiguration")]
-[assembly: InternalsVisibleTo("Steeltoe.Bootstrap.AutoConfiguration.Test")]
 [assembly: InternalsVisibleTo("Steeltoe.Common.Certificates.Test")]
 [assembly: InternalsVisibleTo("Steeltoe.Configuration.ConfigServer")]
 [assembly: InternalsVisibleTo("Steeltoe.Security.Authorization.Certificate")]
--- src/Common/src/Common.Http/Serialization/LongStringJsonConverter.cs
+++ src/Common/src/Common.Http/Serialization/LongStringJsonConverter.cs
@@ -12,15 +12,11 @@ public class LongStringJsonConverter : JsonConverter<long>
 {
     public override long Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        return reader.TokenType == JsonTokenType.Number
-            ? reader.GetInt64()
-            : long.Parse(reader.GetString() ?? throw new ArgumentNullException(nameof(reader)), CultureInfo.InvariantCulture);
+        return reader.TokenType == JsonTokenType.Number ? reader.GetInt64() : long.Parse(reader.GetString(), CultureInfo.InvariantCulture);
     }
 
     public override void Write(Utf8JsonWriter writer, long value, JsonSerializerOptions options)
     {
-        ArgumentGuard.NotNull(writer);
-
         writer.WriteStringValue(value.ToString(CultureInfo.InvariantCulture));
     }
 }
