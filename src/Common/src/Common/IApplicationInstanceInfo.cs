9353c8d72ab3a04434be72d2d78e6e50dcf8034a
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2024-08-09 13:40:36 +0200

    Enable NRT project-wide in Common

--- src/Common/src/Common/IApplicationInstanceInfo.cs
+++ src/Common/src/Common/IApplicationInstanceInfo.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
-#nullable enable
-
 namespace Steeltoe.Common;
 
 /// <summary>
@@ -11,15 +9,15 @@ namespace Steeltoe.Common;
 /// </summary>
 public interface IApplicationInstanceInfo
 {
-    /// <summary>
-    /// Gets the name of this application.
-    /// </summary>
-    string? ApplicationName { get; }
-
     // These properties exist to avoid taking a dependency on Configuration.CloudFoundry in other Steeltoe projects.
     internal string? ApplicationId { get; }
     internal string? InstanceId { get; }
     internal int InstanceIndex { get; }
     internal IList<string> Uris { get; }
     internal string? InternalIP { get; }
+
+    /// <summary>
+    /// Gets the name of this application.
+    /// </summary>
+    string? ApplicationName { get; }
 }
