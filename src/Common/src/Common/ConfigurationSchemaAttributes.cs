1e5f72873f2784003ea5ae613c5ad42c9aa3eb49
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2024-08-09 13:57:33 +0200

    Enable NRT solution-wide

--- src/Common/src/Common/ConfigurationSchemaAttributes.cs
+++ src/Common/src/Common/ConfigurationSchemaAttributes.cs
@@ -1,5 +1,3 @@
-#nullable enable
-
 #pragma warning disable SA1636
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
