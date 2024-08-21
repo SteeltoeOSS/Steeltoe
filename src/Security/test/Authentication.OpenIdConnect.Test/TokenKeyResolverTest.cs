53b01f9a3d77b8b027b71b0f621d8540e69e9056
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2024-08-21 17:18:13 +0200

    Review Resharper warning suppressions

--- src/Security/test/Authentication.OpenIdConnect.Test/TokenKeyResolverTest.cs
+++ src/Security/test/Authentication.OpenIdConnect.Test/TokenKeyResolverTest.cs
@@ -6,16 +6,14 @@ using System.Net;
 using Microsoft.IdentityModel.Tokens;
 using Moq;
 
+// ReSharper disable StringLiteralTypo
+
 namespace Steeltoe.Security.Authentication.OpenIdConnect.Test;
 
 public sealed class TokenKeyResolverTest
 {
-    // ReSharper disable StringLiteralTypo
     private const string Token =
         "eyJhbGciOiJSUzI1NiIsImtpZCI6ImxlZ2FjeS10b2tlbi1rZXkiLCJ0eXAiOiJKV1QifQ.eyJqdGkiOiI0YjM2NmY4MDdlMjU0MzlmYmRkOTEwZDc4ZjcwYzlhMSIsInN1YiI6ImZlNmExYmUyLWM5MTEtNDM3OC05Y2MxLTVhY2Y1NjA1Y2ZjMiIsInNjb3BlIjpbImNsb3VkX2NvbnRyb2xsZXIucmVhZCIsImNsb3VkX2NvbnRyb2xsZXJfc2VydmljZV9wZXJtaXNzaW9ucy5yZWFkIiwidGVzdGdyb3VwIiwib3BlbmlkIl0sImNsaWVudF9pZCI6Im15VGVzdEFwcCIsImNpZCI6Im15VGVzdEFwcCIsImF6cCI6Im15VGVzdEFwcCIsImdyYW50X3R5cGUiOiJhdXRob3JpemF0aW9uX2NvZGUiLCJ1c2VyX2lkIjoiZmU2YTFiZTItYzkxMS00Mzc4LTljYzEtNWFjZjU2MDVjZmMyIiwib3JpZ2luIjoidWFhIiwidXNlcl9uYW1lIjoiZGF2ZSIsImVtYWlsIjoiZGF2ZSIsImF1dGhfdGltZSI6MTQ3MzYxNTU0MSwicmV2X3NpZyI6IjEwZDM1NzEyIiwiaWF0IjoxNDczNjI0MjU1LCJleHAiOjE0NzM2Njc0NTUsImlzcyI6Imh0dHBzOi8vdWFhLnN5c3RlbS50ZXN0Y2xvdWQuY29tL29hdXRoL3Rva2VuIiwiemlkIjoidWFhIiwiYXVkIjpbImNsb3VkX2NvbnRyb2xsZXIiLCJteVRlc3RBcHAiLCJvcGVuaWQiLCJjbG91ZF9jb250cm9sbGVyX3NlcnZpY2VfcGVybWlzc2lvbnMiXX0.Hth_SXpMAyiTf--U75r40qODlSUr60U730IW28K2VidEltW3lN3_CE7HkSjolRGr-DYuWHRvy3i_EwBfj1WTkBaXL373UzPVvNBnat9Gi-vjz07LwmBohk3baG1mmlL8IoGbQwtsmfUPhmO5C6_M4s9wKmTf9XIZPVo_w7zPJadrXfHLfx6iQob7CYpTTix2VBWya29iL7kmD1J1UDT5YRg2J9XT30iFuL6BvPQTkuGnX3ivDuUOSdxM8Z451i0VJmc0LYFBCLJ-Tz6bJ2d0wrtfsbCfuNtxjmGJevcL2jKQbEoiliYj60qNtZdT-ijGUdZjE9caxQ2nOkDkowacpw";
-
-    private static readonly SecurityToken MockToken = new Mock<SecurityToken>().Object;
-    private static readonly TokenValidationParameters MockParameters = new Mock<TokenValidationParameters>().Object;
 
     private const string KeySet = """
         {
@@ -32,6 +30,9 @@ public sealed class TokenKeyResolverTest
           ]
         }
         """;
+
+    private static readonly SecurityToken MockToken = new Mock<SecurityToken>().Object;
+    private static readonly TokenValidationParameters MockParameters = new Mock<TokenValidationParameters>().Object;
 
     [Fact]
     public void ResolveSigningKey_FindsExistingKey()
