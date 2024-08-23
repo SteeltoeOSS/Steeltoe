71f0ec66685eb49cbc20e21fb0cfc20b1877a936
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2024-08-23 09:59:22 +0200

    Fix IDE0130: Namespace does not match folder structure

--- src/Logging/test/DynamicLogger.Test/TestClass.cs
+++ src/Logging/test/DynamicLogger.Test/TestClass.cs
@@ -2,8 +2,9 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
-// ReSharper disable once CheckNamespace
+#pragma warning disable IDE0130 // Namespace does not match folder structure
 
+// ReSharper disable once CheckNamespace
 namespace A.B.C.D;
 
 internal sealed class TestClass;
