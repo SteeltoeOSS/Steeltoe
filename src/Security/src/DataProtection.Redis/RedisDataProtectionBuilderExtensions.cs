9fac1faa44e0d430f3b2ae7acd358840ce800b07
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2024-08-29 17:30:02 +0200

    Fixes for broken Redis data-protection sample, add overload to specify the service binding name (#1349)

--- src/Security/src/DataProtection.Redis/RedisDataProtectionBuilderExtensions.cs
+++ src/Security/src/DataProtection.Redis/RedisDataProtectionBuilderExtensions.cs
@@ -4,11 +4,10 @@
 
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.DataProtection.KeyManagement;
-using Microsoft.AspNetCore.DataProtection.Repositories;
-using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.AspNetCore.DataProtection.StackExchangeRedis;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Options;
+using StackExchange.Redis;
 using Steeltoe.Connectors;
 using Steeltoe.Connectors.Redis;
 
@@ -16,6 +15,12 @@ namespace Steeltoe.Security.DataProtection.Redis;
 
 public static class RedisDataProtectionBuilderExtensions
 {
+    private const string DataProtectionKeysName = "DataProtection-Keys";
+
+    private static readonly Action<RedisCacheOptions> EmptyAction = _ =>
+    {
+    };
+
     /// <summary>
     /// Configures the data protection system to persist keys in a Redis database, using the Steeltoe Connector for Redis.
     /// </summary>
@@ -27,18 +32,60 @@ public static class RedisDataProtectionBuilderExtensions
     /// </returns>
     public static IDataProtectionBuilder PersistKeysToRedis(this IDataProtectionBuilder builder)
     {
+        return PersistKeysToRedis(builder, string.Empty);
+    }
+
+    /// <summary>
+    /// Configures the data protection system to persist keys in a Redis database, using the Steeltoe Connector for Redis.
+    /// </summary>
+    /// <param name="builder">
+    /// The <see cref="IDataProtectionBuilder" /> to configure.
+    /// </param>
+    /// <param name="serviceBindingName">
+    /// The service binding name, in case multiple redis services are used.
+    /// </param>
+    /// <returns>
+    /// The incoming <paramref name="builder" /> so that additional calls can be chained.
+    /// </returns>
+    public static IDataProtectionBuilder PersistKeysToRedis(this IDataProtectionBuilder builder, string serviceBindingName)
+    {
         ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(serviceBindingName);
 
-        builder.Services.TryAddSingleton(provider =>
-        {
-            var connectorFactory = provider.GetRequiredService<ConnectorFactory<RedisOptions, IDistributedCache>>();
-            Connector<RedisOptions, IDistributedCache> connector = connectorFactory.Get();
-            return connector.GetConnection();
-        });
+        builder.Services.AddStackExchangeRedisCache(EmptyAction);
 
-        builder.Services.TryAddSingleton<IXmlRepository, CloudFoundryRedisXmlRepository>();
-        builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IConfigureOptions<KeyManagementOptions>, ConfigureKeyManagementOptions>());
+        builder.Services.AddOptions<RedisCacheOptions>().Configure<ConnectorFactory<RedisOptions, IConnectionMultiplexer>>((options, connectorFactory) =>
+            ConfigureRedisCacheOptions(options, serviceBindingName, connectorFactory));
+
+        builder.Services.AddOptions<KeyManagementOptions>().Configure<ConnectorFactory<RedisOptions, IConnectionMultiplexer>>((options, connectorFactory) =>
+            ConfigureKeyManagementOptions(options, serviceBindingName, connectorFactory));
 
         return builder;
+    }
+
+    private static void ConfigureRedisCacheOptions(RedisCacheOptions options, string serviceBindingName,
+        ConnectorFactory<RedisOptions, IConnectionMultiplexer> connectorFactory)
+    {
+        IConnectionMultiplexer connectionMultiplexer = GetConnectionMultiplexer(connectorFactory, serviceBindingName);
+
+        options.ConnectionMultiplexerFactory = () => Task.FromResult(connectionMultiplexer);
+        options.InstanceName = connectionMultiplexer.ClientName;
+    }
+
+    private static void ConfigureKeyManagementOptions(KeyManagementOptions options, string serviceBindingName,
+        ConnectorFactory<RedisOptions, IConnectionMultiplexer> connectorFactory)
+    {
+        options.XmlRepository = new RedisXmlRepository(() =>
+        {
+            IConnectionMultiplexer connectionMultiplexer = GetConnectionMultiplexer(connectorFactory, serviceBindingName);
+            return connectionMultiplexer.GetDatabase();
+        }, DataProtectionKeysName);
+    }
+
+    private static IConnectionMultiplexer GetConnectionMultiplexer(ConnectorFactory<RedisOptions, IConnectionMultiplexer> connectorFactory,
+        string serviceBindingName)
+    {
+        Connector<RedisOptions, IConnectionMultiplexer> connector = connectorFactory.Get(serviceBindingName);
+        return connector.GetConnection();
     }
 }
