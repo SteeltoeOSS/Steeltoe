6bd352f0c6ab9ada06e29f58391dbc4a40ea224b
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2024-09-20 15:33:10 +0200

    Remove redundant volatile keyword on private fields that are exclusively accessed via Interlocked class

--- src/Management/src/Abstractions/Diagnostics/DiagnosticsManager.cs
+++ src/Management/src/Abstractions/Diagnostics/DiagnosticsManager.cs
@@ -23,7 +23,7 @@ internal sealed class DiagnosticsManager : IObserver<DiagnosticListener>, IDispo
     private IDisposable? _listenersSubscription;
 #pragma warning restore S1450 // Private fields only used as local variables in methods should become local variables
 
-    private volatile int _started;
+    private int _started;
 
     public DiagnosticsManager(IOptionsMonitor<MetricsObserverOptions> observerOptions, IEnumerable<IRuntimeDiagnosticSource> runtimeSources,
         IEnumerable<IDiagnosticObserver> observers, IEnumerable<EventListener> eventListeners, ILogger<DiagnosticsManager> logger)
