655ed4e5ab1c20251095f5bb1a4187ea7116ff8b
Tim Hess <tim.hess@broadcom.com>
2024-10-31 14:23:41 -0500

    Fix ThreadState capitalization

--- src/Management/src/Endpoint/Actuators/ThreadDump/State.cs
+++ src/Management/src/Endpoint/Actuators/ThreadDump/State.cs
@@ -2,16 +2,12 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
-using System.Text.Json.Serialization;
-using Steeltoe.Common.CasingConventions;
-
 #if !DEBUG
 #pragma warning disable SA1602 // Enumeration items should be documented
 #endif
 
 namespace Steeltoe.Management.Endpoint.Actuators.ThreadDump;
 
-[JsonConverter(typeof(SnakeCaseAllCapsEnumMemberJsonConverter))]
 public enum State
 {
     New,
