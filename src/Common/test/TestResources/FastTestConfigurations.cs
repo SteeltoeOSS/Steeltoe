35fac1d6d6d2fd020cb7a6ccf4043b63e8229105
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2025-02-10 09:13:29 +0100

    Fix SA1602 in IDE

--- src/Common/test/TestResources/FastTestConfigurations.cs
+++ src/Common/test/TestResources/FastTestConfigurations.cs
@@ -4,11 +4,29 @@
 
 namespace Steeltoe.Common.TestResources;
 
+/// <summary>
+/// Specifies settings to speed up running tests that don't need the full behavior.
+/// </summary>
 [Flags]
 public enum FastTestConfigurations
 {
+    /// <summary>
+    /// Do not connect to Config Server to fetch configuration.
+    /// </summary>
     ConfigServer = 1,
+
+    /// <summary>
+    /// Do not connect to Eureka/Consul to register the local service instance.
+    /// </summary>
     Discovery = 2,
+
+    /// <summary>
+    /// Use connection strings that fail fast, without relying on a network timeout.
+    /// </summary>
     Connectors = 4,
+
+    /// <summary>
+    /// Enables all available options.
+    /// </summary>
     All = ConfigServer | Discovery | Connectors
 }
