de44b4d1e875d262b454eb3aedd7c17bbb2a3431
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2025-04-03 09:51:07 +0200

    Update to xUnit v3

--- src/Management/test/Endpoint.Test/Actuators/ThreadDump/ThreadDumpEndpointTest.cs
+++ src/Management/test/Endpoint.Test/Actuators/ThreadDump/ThreadDumpEndpointTest.cs
@@ -4,7 +4,6 @@
 
 using Microsoft.Extensions.DependencyInjection;
 using Steeltoe.Management.Endpoint.Actuators.ThreadDump;
-using Xunit.Abstractions;
 
 namespace Steeltoe.Management.Endpoint.Test.Actuators.ThreadDump;
 
