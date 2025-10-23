df31080af0e65165ba3ae22861877f25cb49b3a0
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2025-10-23 17:24:31 +0200

    Capture more diagnostics when memory dump test fails, sync Steeltoe logs with CLR dump logs

--- src/Management/test/Endpoint.Test/Actuators/ThreadDump/EventPipeThreadDumperTest.cs
+++ src/Management/test/Endpoint.Test/Actuators/ThreadDump/EventPipeThreadDumperTest.cs
@@ -35,7 +35,12 @@ public sealed class EventPipeThreadDumperTest
         StackTraceElement? backgroundThreadFrame = threads.SelectMany(thread => thread.StackTrace)
             .FirstOrDefault(frame => frame.MethodName == "BackgroundThreadCallback(class System.Object)");
 
-        backgroundThreadFrame.Should().NotBeNull();
+        if (backgroundThreadFrame == null)
+        {
+            string logs = loggerProvider.GetAsText();
+            throw new InvalidOperationException($"Failed to find expected stack frame. Captured log:{System.Environment.NewLine}{logs}");
+        }
+
         backgroundThreadFrame.IsNativeMethod.Should().BeFalse();
         backgroundThreadFrame.ModuleName.Should().Be(GetType().Assembly.GetName().Name);
         backgroundThreadFrame.ClassName.Should().Be(typeof(NestedType).FullName);
