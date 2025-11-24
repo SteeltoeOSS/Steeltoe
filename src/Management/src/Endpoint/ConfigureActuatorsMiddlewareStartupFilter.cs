1f54c9d29bfa05a60e3e84015a33c25a41b8a570
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2025-11-24 15:10:02 +0100

    Remove middleware warning, it's not reliable

--- src/Management/src/Endpoint/ConfigureActuatorsMiddlewareStartupFilter.cs
+++ src/Management/src/Endpoint/ConfigureActuatorsMiddlewareStartupFilter.cs
@@ -2,27 +2,15 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
-using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Steeltoe.Management.Endpoint.Actuators.CloudFoundry;
 
 namespace Steeltoe.Management.Endpoint;
 
 internal sealed class ConfigureActuatorsMiddlewareStartupFilter : IStartupFilter
 {
-    private readonly ILogger<ConfigureActuatorsMiddlewareStartupFilter> _logger;
-
-    public ConfigureActuatorsMiddlewareStartupFilter(ILogger<ConfigureActuatorsMiddlewareStartupFilter> logger)
-    {
-        ArgumentNullException.ThrowIfNull(logger);
-
-        _logger = logger;
-    }
-
     public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
     {
         ArgumentNullException.ThrowIfNull(next);
@@ -45,30 +33,9 @@ internal sealed class ConfigureActuatorsMiddlewareStartupFilter : IStartupFilter
                 app.UseCloudFoundrySecurity();
             }
 
-            int? beforeMiddlewareCount = GetMiddlewareCount(app);
             next.Invoke(app);
-            int? afterMiddlewareCount = GetMiddlewareCount(app);
-
-            if (beforeMiddlewareCount != afterMiddlewareCount)
-            {
-                _logger.LogWarning(
-                    "Actuators were registered with automatic middleware setup, and at least one additional middleware was registered afterward. This combination is usually undesired. " +
-                    "To remove this warning, either remove the additional middleware registration or set configureMiddleware to false when registering actuators.");
-            }
 
             app.UseActuatorEndpoints();
         };
-    }
-
-    private static int? GetMiddlewareCount(IApplicationBuilder app)
-    {
-        FieldInfo? componentsField = app.GetType().GetField("_components", BindingFlags.NonPublic | BindingFlags.Instance);
-        return componentsField?.GetValue(app) is List<Func<RequestDelegate, RequestDelegate>> components ? components.Count(IsMiddleware) : null;
-    }
-
-    private static bool IsMiddleware(Func<RequestDelegate, RequestDelegate> component)
-    {
-        // This type exists so that ASP.NET Core can identify where to inject UseRouting. It is not a real middleware.
-        return component.Target == null || component.Target.ToString() != "Microsoft.AspNetCore.Builder.WebApplicationBuilder+WireSourcePipeline";
     }
 }
