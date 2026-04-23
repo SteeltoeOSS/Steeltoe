2d615c97868eab671747b032d628e80784238dbf
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2026-04-23 13:55:03 +0200

    Fix broken build

--- src/Configuration/src/CloudFoundry/ServiceBindings/CloudFoundryServiceBrokerTypes.cs
+++ src/Configuration/src/CloudFoundry/ServiceBindings/CloudFoundryServiceBrokerTypes.cs
@@ -10,16 +10,53 @@ namespace Steeltoe.Configuration.CloudFoundry.ServiceBindings;
 [Flags]
 public enum CloudFoundryServiceBrokerTypes
 {
+    /// <summary>
+    /// Don't use any of the built-in brokers.
+    /// </summary>
     None = 0x0,
 
+    /// <summary>
+    /// Use the built-in brokers for Netflix Eureka.
+    /// </summary>
     Eureka = 0x1,
+
+    /// <summary>
+    /// Use the built-in brokers for JWT and OpenID Connect.
+    /// </summary>
     Identity = 0x2,
+
+    /// <summary>
+    /// Use the built-in brokers for MongoDB.
+    /// </summary>
     MongoDb = 0x4,
+
+    /// <summary>
+    /// Use the built-in brokers for MySQL.
+    /// </summary>
     MySql = 0x8,
+
+    /// <summary>
+    /// Use the built-in brokers for PostgreSQL.
+    /// </summary>
     PostgreSql = 0x10,
+
+    /// <summary>
+    /// Use the built-in brokers for RabbitMQ.
+    /// </summary>
     RabbitMQ = 0x20,
+
+    /// <summary>
+    /// Use the built-in brokers for Redis/Valkey.
+    /// </summary>
     Redis = 0x40,
+
+    /// <summary>
+    /// Use the built-in brokers for Microsoft SQL Server.
+    /// </summary>
     SqlServer = 0x80,
 
+    /// <summary>
+    /// Use all built-in brokers.
+    /// </summary>
     All = Eureka | Identity | MongoDb | MySql | PostgreSql | RabbitMQ | Redis | SqlServer
 }
