43b93b623b748ddfe71f5844943325b4b33ce73c
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2026-09-08 13:26:39 +0200

    Update Sonar analyzer to latest version
    Changed rules:
    -S4792       // Configuring loggers is security-sensitive
    *S5042 Warning -> None     // Guard against "Zip Bomb" attacks
    +S8949: Warning      // Use overload that takes CancellationToken
    +S8969: Warning, Steeltoe: None (false positives) // ! operators should not be redundant
    +S8970: Warning      // Don't use ! when nullable disabled
    +S8717 [SonarCloud-only]     // Don't use multiple [Key] attributes in EF Core
    +S8718 [SonarCloud-only]     // About EF Core client-evaluated default values
    +S8733 [SonarCloud-only]    // Use AsSplitQuery in EF Core
    +S8747 [SonarCloud-only]    // Don't narrow column types in EF Core migrations
    +S9022 [SonarCloud-only]    // Duplicate Include calls in EF Core
    +S9129 [SonarCloud-only]    // Merge include chains in EF Core

--- src/Common/src/Hosting/HostBuilderWrapper.cs
+++ src/Common/src/Hosting/HostBuilderWrapper.cs
@@ -148,9 +148,7 @@ internal sealed class HostBuilderWrapper
         }
         else
         {
-#pragma warning disable S4792 // Configuring loggers is security-sensitive
             _configureServicesActions.Add((contextWrapper, collection) => collection.AddLogging(builder => configureAction(contextWrapper, builder)));
-#pragma warning restore S4792 // Configuring loggers is security-sensitive
         }
     }
 
