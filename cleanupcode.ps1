ea7d1e27ea76fb1d45847c9add2f9ad2be96aa71
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2026-09-11 16:17:30 +0200

    Optimize code cleanup

--- cleanupcode.ps1
+++ cleanupcode.ps1
@@ -13,7 +13,7 @@ $solutionFile = 'src/Steeltoe.All.slnx'
 
 dotnet tool restore
 dotnet restore $solutionFile /p:NuGetAudit=false
-dotnet build $solutionFile --no-restore --configuration Release /p:RunAnalyzers=false
+dotnet build $solutionFile --no-restore --configuration Release /p:RunAnalyzers=false /p:GeneratePackageOnBuild=false
 
 if ($revision) {
     $headCommitHash = git rev-parse HEAD
@@ -22,16 +22,16 @@ if ($revision) {
     if ($baseCommitHash -eq $headCommitHash) {
         Write-Output "Running code cleanup on staged/unstaged files."
         dotnet jb cleanupcode --version
-        dotnet regitlint -s $solutionFile --print-command --skip-tool-check --max-runs=5 --jb --dotnetcoresdk=$(dotnet --version) --jb-profile="Steeltoe Full Cleanup" --jb --no-updates --jb --properties:Configuration=Release --jb --properties:RunAnalyzers=false --jb --properties:NuGetAudit=false --jb --verbosity=WARN -f staged,modified
+        dotnet regitlint -s $solutionFile --print-command --skip-tool-check --max-runs=5 --jb --dotnetcoresdk=$(dotnet --version) --jb-profile="Steeltoe Full Cleanup" --jb --no-updates --jb --properties:Configuration=Release --jb --properties:RunAnalyzers=false --jb --properties:NuGetAudit=false --jb --properties:GeneratePackageOnBuild=false --jb --verbosity=WARN -f staged,modified
     }
     else {
         Write-Output "Running code cleanup on commit range $baseCommitHash..$headCommitHash, including staged/unstaged files."
         dotnet jb cleanupcode --version
-        dotnet regitlint -s $solutionFile --print-command --skip-tool-check --max-runs=5 --jb --dotnetcoresdk=$(dotnet --version) --jb-profile="Steeltoe Full Cleanup" --jb --no-updates --jb --properties:Configuration=Release --jb --properties:RunAnalyzers=false --jb --properties:NuGetAudit=false --jb --verbosity=WARN -f staged,modified,commits -a $headCommitHash -b $baseCommitHash
+        dotnet regitlint -s $solutionFile --print-command --skip-tool-check --max-runs=5 --jb --dotnetcoresdk=$(dotnet --version) --jb-profile="Steeltoe Full Cleanup" --jb --no-updates --jb --properties:Configuration=Release --jb --properties:RunAnalyzers=false --jb --properties:NuGetAudit=false --jb --properties:GeneratePackageOnBuild=false --jb --verbosity=WARN -f staged,modified,commits -a $headCommitHash -b $baseCommitHash
     }
 }
 else {
     Write-Output "Running code cleanup on all files."
     dotnet jb cleanupcode --version
-    dotnet regitlint -s $solutionFile --print-command --skip-tool-check --jb --dotnetcoresdk=$(dotnet --version) --jb-profile="Steeltoe Full Cleanup" --jb --no-updates --jb --properties:Configuration=Release --jb --properties:RunAnalyzers=false --jb --properties:NuGetAudit=false --jb --verbosity=WARN
+    dotnet regitlint -s $solutionFile --print-command --skip-tool-check --jb --dotnetcoresdk=$(dotnet --version) --jb-profile="Steeltoe Full Cleanup" --jb --no-updates --jb --properties:Configuration=Release --jb --properties:RunAnalyzers=false --jb --properties:NuGetAudit=false --jb --properties:GeneratePackageOnBuild=false --jb --verbosity=WARN
 }
